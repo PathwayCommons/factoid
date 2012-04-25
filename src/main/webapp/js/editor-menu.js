a4cbb2168bc1265bffb1deb45e4e34b32c624eae
Max Franz <maxkfranz@gmail.com>
2012-04-25 13:11:35 -0400

    revise menu & add factoid info

--- src/main/webapp/js/editor-menu.js
+++ src/main/webapp/js/editor-menu.js
@@ -63,7 +63,7 @@
 		    },
 		    
 		    {
-		    	icon: "ui-icon-noun ui-icon-minus",
+		    	icon: "ui-icon-noun ui-icon-delete",
 		    	//name: $.i18n("menu.delete.name"),
 		    	attr: {
 		    		id: "menubar-delete"
@@ -77,53 +77,56 @@
 		    		});
 		    	},
 		    },
+
+		    { 
+				icon: "ui-icon-noun ui-icon-share",
+				attr: {
+					id: "menubar-layout"
+				},
+				labelAttr: {
+					tooltip: $.i18n("menu.layout.tooltip")
+				},
+				select: function(){ ui.relayout(); }
+			},
 		    
 		    {
-		    	name: $.i18n("menu.more.name"),
-		    	attr: {
-		    		id: "menubar-more"
-		    	},
-		    	labelAttr: {
-		    		tooltip: $.i18n("menu.more.tooltip")
-		    	},
-		    	open: function(){
-		    		$(".ui-tooltip-undo").remove();
-		    	},
-		    	items: [
-		    	    { 
-		    	    	name: $.i18n("menu.layout.name"),
-				    	attr: {
-				    		id: "menubar-layout"
-				    	},
-				    	labelAttr: {
-				    		tooltip: $.i18n("menu.layout.tooltip"),
-				    		tipposition: "left"
-				    	},
-				    	select: function(){ ui.relayout(); }
-		    	    },
-		    	    {
-		    	    	name: $.i18n("menu.show_pan_zoom.name"),
-		    	    	checkable: true,
-		    	    	checked: true,
-				    	attr: {
-				    		id: "menubar-show-pan-zoom"
-				    	},
-				    	labelAttr: {
-				    		tooltip: $.i18n("menu.show_pan_zoom.tooltip"),
-				    		tipposition: "left"
-				    	},
-				    	select: function(on){
-				    		var pz = $("#vis > .ui-cytoscapeweb-panzoom");
-				    		
-				    		if( on ){
-				    			pz.show();
-				    		} else {
-				    			pz.hide();
-				    		}
-				    	}
-		    	    }
-		    	]
-		    }
+				icon: "ui-icon-noun ui-icon-pan",
+				checkable: true,
+				checked: true,
+				attr: {
+					id: "menubar-show-pan-zoom"
+				},
+				labelAttr: {
+					tooltip: $.i18n("menu.show_pan_zoom.tooltip")
+				},
+				select: function(on){
+					var pz = $("#vis > .ui-cytoscapeweb-panzoom");
+					
+					if( on ){
+						pz.show();
+					} else {
+						pz.hide();
+					}
+				}
+			},
+
+			{
+				icon: "ui-icon-noun ui-icon-information-alt-2",
+				attr: {
+					id: "menubar-info"
+				},
+				labelAttr: {
+					tooltip: $.i18n("menu.info.tooltip")
+				},
+				select: function(){
+					$("#menubar-info").commandtip({
+						title: "Factoid - Building the Future of Scientific Publishing",
+						content: "<p>Factoid helps authors to translate their written scientific text into formal descriptions of biological processes useful for sharing their results with others, bioinformatics analysis and integrating with other data to help build a more complete model of a cell.</p>\
+								<p>Factoid aims to be part of the publication process.  With the author's help, it will extract pathway and related information from a paper as it is submitted, submit the resulting diagram as a visual abstract for peer-review along with the paper and finally, publish the information in a sharable and computable format accompanying the paper for others to use.</p>\
+								<p>Factoid 1.0 helps turn text into a simple and editable network model of a biological process. Text is automatically converted to a first draft of a network which can then be corrected using easy to use editing functions.  Future versions will have more advanced text mining functionality to improve the 'first draft', will allow saving the results in standard formats for sharing and will make it easier to add text and edit the network.</p>"
+					});
+				}
+			}
 		]
 	});
 	

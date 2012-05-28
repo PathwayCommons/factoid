b8fa4bbcb0a14bfe3bf8ce4a311a03b4d564b2f1
Max Franz <maxkfranz@gmail.com>
2012-05-28 14:05:20 -0400

    add icon sets

---
+++ model/document.js
@@ -0,0 +1,10 @@
+(function( exports ){
+
+	var expect = require("chai").expect;
+	var __ = require("underscore");
+	var Backbone = require("backbone");
+	var model = require("../model");
+
+	var Document;
+
+})( typeof exports === 'undefined' ? (!this.model ? this.model = {} : this.model) : exports );

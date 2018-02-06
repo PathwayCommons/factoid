c9bc4b5bf58a601f559a400f9510baaa80baf4e9
Dylan Fong <d2fong@uwaterloo.ca>
2018-02-06 14:50:54 -0500

    Basic skeleton for the submit page and the editor page

--- src/client/components/multi-form-editor/editor.js
+++ src/client/components/multi-form-editor/editor.js
@@ -1,9 +1,26 @@
 const React = require('react');
 const h = require('react-hyperscript');
+const { Tab, Tabs, TabList, TabPanel } = require('react-tabs');
 
 class Editor extends React.Component {
   render() {
-    return h('div', 'Editor Page');
+    return h('div.multi-form-editor', [
+      h('div.editor-content', [
+        h(Tabs, [
+          h(TabList, [
+            h(Tab, ['Graph View Tab']),
+            h(Tab, ['Form View Tab']),
+            h(Tab, ['Free Text View Tab'])
+          ]),
+          h(TabPanel, 'Graph View'),
+          h(TabPanel, 'Form View'),
+          h(TabPanel, 'Free Text View')
+        ])
+      ]),
+      h('div.context-side-bar', [
+        'This should show you what to do in the editor'
+      ])
+    ]);
   }
 }
 
--- src/client/components/multi-form-editor/submit-page.js
+++ src/client/components/multi-form-editor/submit-page.js
@@ -3,7 +3,19 @@ const h = require('react-hyperscript');
 
 class SubmitPage extends React.Component {
   render() {
-    return h('div', 'Submit Page');
+    return h('div.submit-page', [
+      h('div.submit-panel', [
+        'View the result to submit here'
+      ]),
+      h('div.submit-context', [
+        h('div.context-box', [
+          'context to explain stuff goes here'
+        ]),
+        h('div.submit-button', [
+          'submit button goes here'
+        ])
+      ])
+    ]);
   }
 }
 

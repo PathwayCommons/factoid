a38531f75e7cdccd4ec2079bab6d77398901c67f
Max Franz <maxkfranz@gmail.com>
2018-02-06 11:30:37 -0500

    Update the landing page with a more concise intro and update the figure to match the slide deck

--- src/client/components/example-document.js
+++ src/client/components/example-document.js
@@ -1,8 +1,7 @@
 const h = require('react-hyperscript');
-const { Link } = require('react-router-dom');
 const { Component } = require('react');
 
-const EXAMPLE_TEXT = `Under normal conditions, p53 is a short-lived protein. The MDM2 protein, usually interacts with p53, and by virtue of its E3 ubiquitin ligase activity, mediates its degradation by the ubiquitin-proteasome machinery. Upon detection of DNA damage, the ATM kinase mediates the phosphorylation of the Mdm2 protein to block its interaction with p53. The p53 protein activates the transcription of cyclin-dependent kinase inhibitor, p21. p21 inactivates the CyclinE:Cdk2 complexes, which prevents entry into S-phase of the cell cycle.`;
+const EXAMPLE_TEXT = `Upon detection of DNA damage, the ATM kinase mediates the phosphorylation of the Mdm2 protein to block its interaction with p53. The p53 protein activates the transcription of cyclin-dependent kinase inhibitor, p21. p21 inactivates the CCNE1:Cdk2 complex.`;
 
 class ExampleDocument extends Component {
   constructor( props ){
--- src/client/components/home.js
+++ src/client/components/home.js
@@ -10,16 +10,21 @@ class Home extends Component {
         ' Factoid'
       ]),
       h('h3', 'Share your pathway with the world'),
-      h('p', 'Biological pathways are invaluable tools that help researchers consider how cellular activities arise from interactions between multiple components. Unfortunately, most pathways are available only in ‘human-readable’ form within publications, which limits them from being easily shared and discovered. Factoid captures pathway knowledge in three easy steps:'),
+      h('p', `
+        Factoid gives your biological research visibility by linking your paper to digital data for your pathway.
+
+        We do this in four easy steps:
+      `),
       h('p', [ h('span.home-steps-figure') ]),
       h('ol', [
-        h('li', 'The author first receives an email from Factoid, with a link to edit the Factoid document associated with the paper.  '),
+        h('li', 'The paper is submitted to the journal.'),
+        h('li', 'The author receives an email from Factoid, with a link to edit the Factoid document associated with the paper.  '),
         h('li', [
-          h('span', 'Next, the author edits the document (e.g.'),
+          h('span', 'Next, the author edits the document (e.g. '),
           h(Link, { className: 'plain-link', to: '/example-document' }, 'sample document'),
           h('span', ') to ensure that it accurately portrays the paper.'),
         ]),
-        h('li', 'The author then finalises the document, and the data is automatically sent to the journal.  ')
+        h('li', 'The author then finalises the document, and the data is automatically sent to the journal.  The data is publicly available for anyone to use and cite.')
       ])
     ]);
   }

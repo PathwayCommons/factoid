47f8f657db6c4a81ccea951d7ceb0b70115abab3
Max Franz <maxkfranz@gmail.com>
2018-03-08 18:41:26 -0500

    Send HTTP 500 on failed element association queries

--- src/server/routes/api/element-association/index.js
+++ src/server/routes/api/element-association/index.js
@@ -3,11 +3,19 @@ const jsonifyResult = response => ( result => response.json( result ) );
 const http = require('express').Router();
 
 http.get('/search', function( req, res ){
-  provider.search( req.query ).then( jsonifyResult(res) );
+  (
+    provider.search( req.query )
+    .then( jsonifyResult(res) )
+    .catch( err => res.status(500).send(err) )
+  );
 });
 
 http.get('/get', function( req, res ){
-  provider.get( req.query ).then( jsonifyResult(res) );
+  (
+    provider.get( req.query )
+    .then( jsonifyResult(res) )
+    .catch( err => res.status(500).send(err) )
+  );
 });
 
 module.exports = http;

468975cebfe73d0670cf36c0cb8481aeb3d1e2a8
Dylan Fong <d2fong@uwaterloo.ca>
2018-05-23 15:21:34 -0400

    add text for the editor chooser

--- src/client/components/document-view-chooser.js
+++ src/client/components/document-view-chooser.js
@@ -19,14 +19,17 @@ class DocumentViewChooser extends React.Component {
 
     let rootChildren = [
       h('h1.view-chooser-title', 'Choose editor'),
+      h('p', 'Your text has been converted into digital networks that now can be explored, modified and expanded through different editors.'),
       h('div.document-view-chooser-radios', [
         h('div.document-view-chooser-choice', { onClick: () => this.goToEditor('document') }, [
           h('div.network-choice'),
-          h('label', { htmlFor: 'document-view-chooser-network' }, 'Network Editor')
+          h('h2', { htmlFor: 'document-view-chooser-network' }, 'Network Editor'),
+          h('p', 'View and edit your network graphically.  Biochemical entities are represented as circles, interactions are represented by edges connecting two etnities.')
         ]),
         h('div.document-view-chooser-choice', { onClick: () => this.goToEditor('form') }, [
           h('div.form-choice'),
-          h('label', { htmlFor: 'document-view-chooser-form' }, 'Form Editor')
+          h('h2', { htmlFor: 'document-view-chooser-form' }, 'Form Editor'),
+          h('p', 'Add new types of interactions to your document with easy to use form template text entries.')
         ])
       ]),
       h(DocumentWizardStepper, {

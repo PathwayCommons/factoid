c27c2a21f13d1d7b7af272c7a6357d47e7741366
Dylan Fong <d2fong@uwaterloo.ca>
2018-06-04 15:36:42 -0400

    display 50+ when there are more than 50 todo items - task number indicator position fixes

--- src/client/components/editor/buttons.js
+++ src/client/components/editor/buttons.js
@@ -61,93 +61,6 @@ class EditorButtons extends React.Component {
       ]),
     ]);
 
-    grs.push([
-      h(Tooltip, { description: 'Help' }, [
-        h('button.editor-button.plain-button', { onClick: () => bus.emit('togglehelp') }, [
-          h('i.material-icons', 'info')
-        ])
-      ]),
-
-      h(Tooltip, _.assign({}, baseTooltipProps,  { description: 'Tasks' }), [
-        h(Toggle, {
-          className: 'editor-button plain-button task-list-button',
-          controller,
-          document,
-          bus,
-          onToggle: () => controller.toggleTaskListMode(),
-          getState: () => controller.taskListMode()
-        }, [
-          h(TaskListButton, { controller, document, bus })
-        ])
-      ]),
-
-      h(Popover, {
-        tippy: {
-          position: 'right',
-          html: h('div.editor-linkout', [
-            h(Linkout, { document })
-          ])
-        }
-      }, [
-        h(Tooltip, _.assign({}, baseTooltipProps, { description: 'Share link' }), [
-          h('button.editor-button.plain-button', [
-            h('i.material-icons', 'link')
-          ])
-        ])
-      ]),
-
-      h(Tooltip, _.assign( {}, baseTooltipProps, { description: 'Export to Biopax' }), [
-        h('button.editor-button.plain-button', { onClick: () => exportDocumentToOwl(document.id()) }, [
-          h('i.material-icons', 'save_alt')
-        ])
-      ]),
-
-      h(Popover, {
-        tippy: {
-          position: 'right',
-          html: h('div.editor-more-menu', [
-            h('div.editor-more-menu-items', [
-              h('button.editor-more-button.plain-button', {
-                onClick: () => history.push('/new')
-              }, [
-                h('span', ' New factoid')
-              ]),
-              h('button.editor-more-button.plain-button', {
-                onClick: () => history.push('/documents')
-              }, [
-                h('span', ' My factoids')
-              ]),
-              h('button.editor-more-button.plain-button', {
-                onClick: () => {
-                  let id = document.id();
-                  let secret = document.secret();
-
-                  if( document.editable() ){
-                    history.push(`/form/${id}/${secret}`);
-                  } else {
-                    history.push(`/form/${id}`);
-                  }
-                }
-              }, [
-                h('span', ' Form-based editor')
-              ]),
-              h('button.editor-more-button.plain-button', {
-                onClick: () => history.push('/')
-              }, [
-                h('span', ' About & contact')
-              ])
-            ])
-          ])
-        }
-      }, [
-        h(Tooltip, _.assign({}, baseTooltipProps, { description: 'More tools' }), [
-          h('button.editor-button.plain-button', [
-            h('i.material-icons', 'more_vert')
-          ])
-        ])
-      ])
-    ]);
-
     return h(`div.${className}`, grs.map( btns => h('div.editor-button-group', btns) ));
   }
 }
@@ -195,9 +108,31 @@ class AppButtons extends React.Component {
     }
 
     grs.push(appButtons);
+    appButtons.push([
+      h(Popover, {
+        tippy: {
+          position: 'right',
+          html: h('div.editor-linkout', [
+            h(Linkout, { document })
+          ])
+        }
+      }, [
+        h(Tooltip, _.assign({}, baseTooltipProps, { description: 'Share link' }), [
+          h('button.editor-button.plain-button', [
+            h('i.material-icons', 'link')
+          ])
+        ])
+      ]),
+
+      h(Tooltip, _.assign( {}, baseTooltipProps, { description: 'Export to Biopax' }), [
+        h('button.editor-button.plain-button', { onClick: () => exportDocumentToOwl(document.id()) }, [
+          h('i.material-icons', 'save_alt')
+        ])
+      ]),
+    ]);
 
     if( document.editable() ){
-      grs.push([
+      appButtons.push([
         h(Popover, {
           tippy: {
             position: 'right',

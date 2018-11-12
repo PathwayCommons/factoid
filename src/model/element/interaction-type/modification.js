909002de326bed8bbfff8488f8b4fdc25935169f
Max Franz <maxkfranz@gmail.com>
2018-11-12 11:37:13 -0500

    Use old name for PTM

--- src/model/element/interaction-type/modification.js
+++ src/model/element/interaction-type/modification.js
@@ -4,7 +4,7 @@ const { ENTITY_TYPE } = require('../entity-type');
 const { BIOPAX_TEMPLATE_TYPE, BIOPAX_CONTROL_TYPE } = require('./biopax-type');
 
 const VALUE = 'modification';
-const DISPLAY_VALUE = 'Modification/activity change';
+const DISPLAY_VALUE = 'Post-translational modification';
 
 class Modification extends InteractionType {
   constructor( intn ){

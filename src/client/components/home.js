4e2f46e991c11c33471df9ca839bd9fbca611f47
jvwong <jvwong@gmail.com>
2019-05-08 15:26:13 -0400

    Polishing text and updating video.

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -16,18 +16,15 @@ class Home extends Component {
           h('div.hero-title-container#about', [
             h('div.hero-title', 'Factoid')
           ]),
-          h('div.hero-subtitle', 'Smart figures for your papers'),
-          h('div.hero-description', 'Capture your biological interactions in a shareable, collaborative, interactive figure.')
+          h('div.hero-subtitle', 'Uniting research'),
+          h('div.hero-description', 'Add your research findings to a connected map of cellular networks that everyone can search, explore and link to your article.')
         ])
       ]),
       h('section.home-section#why', [
         h('div.page-content', [
-          h('h2.section-title', 'Our mission'),
-          h('h2', 'Make the findings in your article easy to discover'),
-          h('p', `Your figure can be discovered and explored by other researchers, because it's smart:  It's more than just a picture.   What other papers mention my gene?  What other papers mention a particular interaction?   What other papers are related to my research?    Factoid gives you that.`),
           h('div.video-embed', [
             h('iframe.video-embed-iframe', {
-              src: 'https://www.youtube.com/embed/GdeMi5RFNTU',
+              src: 'https://www.youtube.com/embed/HDW9AVYifxQ',
               frameBorder: 0,
               allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
               allowFullScreen: true
@@ -39,7 +36,7 @@ class Home extends Component {
         h('div.page-content', [
           h('h2.section-title', 'For authors'),
           h('h2', 'Put your research where people can find it'),
-          h('p', 'Your publication is the culmination of a tremendous persistent effort spanning many years. Traditional methods dictate that your research will only be discoverable via search engine with a specific set of keywords. Using Factoid will increase the number of ways that people can access your research.')
+          h('p', 'Your paper contains text and figures crafted for readers which makes the knowledge within it difficult to access with a search engine or resuse directly. Factoid solves this problem by helping you create a digital summary of key interactions between genes. Researchers who access your interactions can easily link to your paper, making it more visible to the research community.')
         ])
       ]),
       h('section.home-section#why-publishers', [
@@ -47,7 +44,7 @@ class Home extends Component {
           h('h2.section-title', 'For journals'),
           h('h2', 'Enhance your content and increase your reach'),
           h('p', 'Factoid brings your content to the fore by making it more discoverable and interactive.  Foster deep connections with the researchers you work with by embedding article content within the interaction information they access for their day-to-day research.'),
-          h('p', 'Integrating Factoid in your manuscript workflow is simple and lightweight.  Providing you with a plethora of benefits for minimal investment.')        ])
+          h('p', 'Integrating Factoid in your manuscript workflow is simple and lightweight.')])
       ]),
       h('section.home-section#credits', [
         h('div.page-content.credits', [

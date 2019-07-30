26c629ba5c3ffa7ef59a44344c9f402ff4ba36cf
Max Franz <maxkfranz@gmail.com>
2019-07-30 12:33:57 -0400

    Do not send undefined namespace to the grounding service

--- src/server/routes/api/element-association/grounding-search.js
+++ src/server/routes/api/element-association/grounding-search.js
@@ -29,13 +29,18 @@ const searchAll = opts => query( opts, 'search' );
 const search = opts => {
   let { limit, offset, namespace, name, organismCounts } = opts;
 
-  let queryOpts = { namespace, q: name };
+  let queryOpts = { q: name };
+
   if ( organismCounts ) {
     let cmp = ( a, b ) => organismCounts[ b ] - organismCounts[ a ];
     let nonZero = taxonId => organismCounts[taxonId] !== 0;
     let organismOrdering = Object.keys( organismCounts ).filter( nonZero ).sort( cmp );
 
     queryOpts.organismOrdering = organismOrdering;
+  }
+
+  if( namespace ){
+    queryOpts.namespace = namespace;
   }
 
   return searchAll( queryOpts )

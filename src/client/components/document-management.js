c142cfcfc362406575c3a186476014a9c3f4c8bc
jvwong <jvwong@gmail.com>
2019-10-23 11:59:04 -0400

    Email enabled. Updated styling for footer.

--- src/client/components/document-management.js
+++ src/client/components/document-management.js
@@ -4,20 +4,72 @@ import h from 'react-hyperscript';
 import queryString from 'query-string';
 import { Link } from 'react-router-dom';
 
-// import { BASE_URL } from '../../config';
+import { 
+  BASE_URL,
+  EMAIL_FROM,
+  EMAIL_FROM_ADDR,
+  EMAIL_VENDOR_MAILJET,
+  INVITE_TMPLID,
+  EMAIL_CONTEXT_SIGNUP
+} from '../../config' ;
 //import { tryPromise } from '../../util';
 import MainMenu from './main-menu';
 
-const encodeApiKey = secret => secret ? secret: '%27%27';
+const sanitize = secret => secret === '' ? '%27%27': secret;
 const LOCALE = 'en-US';
-const DEFAULT_DATE_OPTS = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const DEFAULT_TIME_OPTS = { hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+const DEFAULT_DATE_OPTS = { year: 'numeric', month: 'long', day: 'numeric' };
+const DEFAULT_TIME_OPTS = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
 
-const getDocs = apiKey => {
+const getDocs = apiKey => {console.log(apiKey);
   const url = '/api/document';
   const params = { apiKey };
   const paramsString = queryString.stringify( params );
   return fetch(`${url}?${paramsString}`);
+};
+
+const sendMail = ( docOpts, apiKey ) => {
+  const url = '/api/document/email';
+  const defaults = {
+    from: { 
+      name: EMAIL_FROM,
+      address: EMAIL_FROM_ADDR
+    },
+    template: {
+      vendor: EMAIL_VENDOR_MAILJET
+    }
+  };
+
+  const opts = _.merge( {}, defaults, docOpts );
+  const data = { opts, apiKey };
+  
+  return fetch( url, {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json' 
+    },
+    body: JSON.stringify( data )
+  });
+};
+
+const msgFactory = doc => {
+
+  const msgOpts = {
+    to: {
+      address: `${doc.contributorEmail}`
+    },     
+    subject: 'Your invitation to Biofactoid is ready',
+    template: {
+      id: INVITE_TMPLID,	
+      vars: {	
+        citation: `${doc.title}\n${doc.authors}\n${doc.journalName}`,
+        privateUrl: `${BASE_URL}/document/${doc.id}/${doc.secret}`,
+        context: EMAIL_CONTEXT_SIGNUP
+      }	
+    }
+  };
+
+  return msgOpts;
 };
 
 class DocumentManagement extends React.Component {
@@ -34,40 +86,35 @@ class DocumentManagement extends React.Component {
 
   componentDidMount(){
     const query = queryString.parse( this.props.history.location.search );
-    if( query.apiKey ) this.updateDocs( encodeApiKey( query.apiKey ) ); // for convenience, put it as query param
+    if( query.apiKey ) this.updateDocs( query.apiKey ); // for convenience, put it as query param
   }
 
-  updateDocs( apiKey = this.state.apiKey ){
-    getDocs( encodeApiKey( apiKey ) )
+  updateDocs( apiKey = this.state.apiKey ){console.log(apiKey);
+    getDocs( apiKey )
       .then( res => res.json() )
       .then( docs => new Promise( resolve => {
           this.setState({
             validApiKey: true, // no error means its good
+            apiKey,
             error: null,
             docs }, resolve);
         })
       )
-      .then( () => this.props.history.push(`/document?apiKey=${apiKey}`) )
+      .then( () => this.props.history.push(`/document?apiKey=${sanitize(apiKey)}`) )
       .catch( e => {
-        this.setState( { error: e, validApiKey: false } );
+        this.setState( { 
+          error: e, 
+          validApiKey: false,
+          apiKey: ''
+         }, () => this.props.history.push(`/document`) );
         return;
       });
   }
 
-  email( ) {
-    // const inviteOpts = {
-    //   subject: 'Your invitation to Biofactoid',
-    //   citation: `${doc.title}\n${doc.journalName}`,
-    //   privateUrl: `${BASE_URL}/${doc.id}/${doc.secret}`
-    // };
-
-    // const mailOpts = _.assign( {}, {
-    //   to: doc.contributorEmail
-    // });
-
-    //TODO setup api route?
-    // console.log( `email sent to ${JSON.stringify(doc.contributorEmail)}`);
-    return {};
+  email( doc ) {  
+    const mailOpts = msgFactory( doc );
+    sendMail( mailOpts, this.state.apiKey )
+      .then( info => console.log(info));
   }
 
   handleFormChange( apiKey ) {
@@ -82,8 +129,6 @@ class DocumentManagement extends React.Component {
   render(){
     let { history } = this.props;
     let { docs, validApiKey } = this.state;
-
-
 
     const withDefault = ( o, k ) => _.get( o, k ) ? _.get( o, k ) : `No ${k}`;
     const formatDate = dString => {
@@ -131,49 +176,75 @@ class DocumentManagement extends React.Component {
         }, 'Email Invite' );
     };
 
-    const contributorInfo = doc => {
+    const correspondence = doc => {
       return [
-        h( 'p', `Contributor: ${doc.contributorName} (${doc.contributorEmail})` ),
-        contributorActions( doc )
+        h( 'div.document-management-horizontal-list', [          
+          h( 'ul', [
+            h( 'li', [ 
+              h( 'button', {
+                onClick: () => this.email( doc, context )
+              }, 'Email Invite' )
+            ])
+          ])
+        ])
       ];
     };
 
-    // Document Status
+    
+
+    // Document -- Views
+    const documentViews = doc => {
+      const summaryPath = `/document/${doc.id}`;
+      const editPath = `${summaryPath}/${doc.id}`;
+      return [
+        h( 'div.document-management-horizontal-list', [          
+          h( 'ul', [
+            h( 'li', [ 
+              h( Link, {
+                to: summaryPath,
+                target: '_blank',
+              }, 'Summary' )
+            ]),
+            h( 'li', [ 
+              h( Link, {
+                to: editPath,
+                target: '_blank'
+              }, 'Editable' )
+            ])
+          ])
+        ])
+      ];
+    };
+
+    // Document Footer
     const hasSubmitted = o => _.has( o, ['data', 'submitted'] );
     const submitDate = doc => {
       if ( _.has( doc, 'submitted' ) ) {
         const submitOp = _.find( doc._ops, hasSubmitted );
         const submitDate = formatDate( _.get( submitOp, 'timestamp' ) );
-        return h( 'p.emphasize', { key: 'submitted' }, `Submitted: ${submitDate}` );
+        return h( 'li', { key: 'submitted' }, `Submitted: ${submitDate}` );
       }
       return null;
     };
 
-    const documentStatus = doc => {
+    const documentFooter = doc => {
       return [
-        h( 'p', { key: '_creationTimestamp' }, `Created: ${formatDate( doc._creationTimestamp )}` ),
-        submitDate( doc )
-        // TODO: [Last updated], ...
-      ];
-    };
-
-    // Document -- Action
-    const documentAction = doc => {
-      return [
-        h( Link, {
-            to: `/document/${doc.id}`,
-            target: '_blank'
-          }, h( 'button', ['View'] ) )
+        h( 'ul', [
+          h( 'li.mute', { key: '_creationTimestamp' }, `Created: ${formatDate( doc._creationTimestamp )}` ),
+          submitDate( doc )
+          // TODO: [Last updated], ...
+        ])
       ];
     };
 
     const documentList = h( 'ul', [
-      docs.map( doc =>
+      docs.map( ( doc, i ) =>
         h( 'li', { key: doc.id }, [
           h( 'div.document-management-document-section', articleInfo( doc ) ),
-          h( 'div.document-management-document-section', documentStatus( doc ) ),
-          h( 'div.document-management-document-section', contributorInfo( doc ) ),
-          h( 'div.document-management-document-section', documentAction( doc ) )
+          h( 'div.document-management-document-section', documentViews( doc ) ),
+          h( 'div.document-management-document-section', correspondence( doc ) ),
+          h( 'div.document-management-document-footer', documentFooter( doc ) ),
+          h( 'hr' )
         ]
       ))
     ]);

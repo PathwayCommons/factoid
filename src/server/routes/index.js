90de8eb61b237e3242b464cfecef0d417f7036d8
Max Franz <maxkfranz@gmail.com>
2019-11-14 15:12:22 -0500

    Fixes non-production `twitter:image` meta tags
    The tags previously had the production base URL hard-coded.  The URL is now dynamic and the variable names are made consistent with the env vars.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import { getDocumentJson } from './api/document';
-import { TWITTER_ACCOUNT_NAME } from '../../config';
+import { TWITTER_ACCOUNT_NAME, BASE_URL } from '../../config';
 
 const http = express.Router();
-const twitterAccount = TWITTER_ACCOUNT_NAME;
 
 // get the app ui but with static share metadata in the html
 const getDocumentPage = function(req, res) {
   ( getDocumentJson(req.params.id)
-    .then(document => res.render('index.html.ejs', { document, twitterAccount }))
+    .then(document => res.render('index.html.ejs', {
+      document,
+      TWITTER_ACCOUNT_NAME,
+      BASE_URL
+    }))
   );
 };
 

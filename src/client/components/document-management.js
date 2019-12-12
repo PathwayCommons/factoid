98bd051e5ba80fce6d98f686905bb259aa4f81dc
Jeffrey Wong <jvwong@outlook.com>
2019-12-12 16:00:44 -0500

    Enable email buttons whenever the doc is approved or submitted.

--- src/client/components/document-management.js
+++ src/client/components/document-management.js
@@ -294,14 +294,14 @@ class DocumentManagement extends DirtyComponent {
             doc,
             type: CORRESPONDENCE_INVITE_TYPE,
             label: _.capitalize( CORRESPONDENCE_INVITE_TYPE ),
-            disableWhen: doc.requested() || doc.trashed() || doc.submitted(),
+            disableWhen: doc.requested(),
             apiKey: this.state.apiKey
           }),
           h( EmailButtton, {
             doc,
             type: CORRESPONDENCE_FOLLOWUP_TYPE,
             label: _.upperFirst( CORRESPONDENCE_FOLLOWUP_TYPE.replace(/([A-Z])/g, (match, letter) => '-' + letter) ),
-            disableWhen: doc.requested() || doc.trashed() || !doc.submitted(),
+            disableWhen: doc.requested(),
             apiKey: this.state.apiKey
           })
         ]);

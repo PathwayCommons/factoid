ee6cd602b7e72c46f5d27c315c52fa3bef66eeef
Jeffrey Wong <jvwong@outlook.com>
2019-12-17 09:47:20 -0500

    - Remove uid checking on fetchPubmed service - Accept uids with decimals on signup filtering for paperId

--- src/server/routes/api/document/pubmed/index.js
+++ src/server/routes/api/document/pubmed/index.js
@@ -18,33 +18,33 @@ const findPubmedId = async paperId => {
   if( !_.isString( paperId ) ) throw new TypeError( errMessage );
   const isUidLike = digitsRegex.test( paperId );
   const isPubMedUrlLike = paperId.startsWith( PUBMED_LINK_BASE_URL );
-  
+
   if( isUidLike ){
     id = paperId;
-    
+
   } else if( isPubMedUrlLike ){
     const pubmedUrl = new URL( PUBMED_LINK_BASE_URL );
     const paperIdUrl = new URL( paperId );
     const isSameHost = paperIdUrl.hostname === pubmedUrl.hostname;
-    
-    const pathUidMatchResult = paperIdUrl.pathname.match( /^\/pubmed\/(\d+)$/ );
+
+    const pathUidMatchResult = paperIdUrl.pathname.match( /^\/pubmed\/([0-9.]+)$/ );
     const paperIdUrlSearchTerm = paperIdUrl.searchParams.get('term');
-    
+
     if( isSameHost && !_.isNull( pathUidMatchResult ) ){
       id = pathUidMatchResult[1];
-      
+
     } else if( isSameHost && paperIdUrlSearchTerm ) {
       const { searchHits, count } = await searchPubmed( paperIdUrlSearchTerm );
-      if( count === 1 ){ 
+      if( count === 1 ){
         id = _.first( searchHits );
       } else {
         throw new TypeError( errMessage );
       }
-      
+
     } else {
       throw new TypeError( errMessage );
     }
-    
+
   } else {
     throw new TypeError( errMessage );
   }
@@ -54,18 +54,18 @@ const findPubmedId = async paperId => {
 
 /**
  * getPubmedRecord
- * 
- * Retrieve a single PubmedArticle. Shall interpret an input as either: 
- *   - A set of digits 
- *   - A url 
+ *
+ * Retrieve a single PubmedArticle. Shall interpret an input as either:
+ *   - A set of digits
+ *   - A url
  *     - Containing a set of digits e.g. 'https://www.ncbi.nlm.nih.gov/pubmed/123456'
- *     - A search returning a single PubMed UID 
- * 
+ *     - A search returning a single PubMed UID
+ *
  * @param {String} paperId Contains or references a single PubMed uid (see above). If 'demo' return canned demo data.
  * @return {Object} The unique PubMedArticle (see [NLM DTD]{@link https://dtd.nlm.nih.gov/ncbi/pubmed/out/pubmed_190101.dtd} )
  * @throws {TypeError} When paperId falls outside of the above cases
  */
-const getPubmedArticle = async paperId => { 
+const getPubmedArticle = async paperId => {
   if( paperId === DEMO_ID ) return demoPubmedArticle;
   const candidateId = await findPubmedId( paperId );
   const { PubmedArticleSet } = await fetchPubmed({
@@ -79,4 +79,4 @@ const getPubmedArticle = async paperId => {
   }
 };
 
-export { getPubmedArticle }; 
+export { getPubmedArticle };

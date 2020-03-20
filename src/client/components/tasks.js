ceb1e2c7daccc0af7470ef1ab7cd0c1be543e947
Max Franz <maxkfranz@gmail.com>
2020-03-20 11:27:26 -0400

    The notification list should use the same element set as `incompleteEles`

--- src/client/components/tasks.js
+++ src/client/components/tasks.js
@@ -97,7 +97,7 @@ class TaskView extends DataComponent {
       return !ele.completed() && !ele.isInteraction() && ele.type() !== ENTITY_TYPE.COMPLEX;
     });
 
-    let ntfns = document.entities().filter(ele => !ele.completed()).map(ele => {
+    let ntfns = incompleteEles.map(ele => {
       let entMsg = ele => `${ele.name() === '' ? 'unnamed entity' : ele.name() + (ele.completed() ? '' : '') }`;
       let innerMsg = entMsg(ele);
 

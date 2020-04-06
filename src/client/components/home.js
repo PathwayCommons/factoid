546ef51ab6a37ce3d1774b7def209ccf604c8ff9
Max Franz <maxkfranz@gmail.com>
2020-04-06 14:11:18 -0400

    Links on the home page go out to new tabs

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -435,11 +435,11 @@ class Home extends Component {
                   html: h('div.home-contact-info', [
                     h('p', [
                       'Biofactoid is freely brought to you in collaboration with ',
-                      h('a.plain-link', { href: 'https://baderlab.org' }, 'Bader Lab at the University of Toronto'),
+                      h('a.plain-link', { href: 'https://baderlab.org', target: '_blank' }, 'Bader Lab at the University of Toronto'),
                       ', ',
-                      h('a.plain-link', { href: 'http://sanderlab.org' }, 'Sander Lab at Harvard'),
+                      h('a.plain-link', { href: 'http://sanderlab.org', target: '_blank' }, 'Sander Lab at Harvard'),
                       ', and the ',
-                      h('a.plain-link', { href: 'https://www.ohsu.edu/people/emek-demir/AFE06DC89ED9AAF1634F77D11CCA24C3' }, 'Pathway and Omics Lab at the University of Oregon'),
+                      h('a.plain-link', { href: 'https://www.ohsu.edu/people/emek-demir/AFE06DC89ED9AAF1634F77D11CCA24C3', target: '_blank' }, 'Pathway and Omics Lab at the University of Oregon'),
                       '.'
                     ]),
                     h('p', [
@@ -453,8 +453,8 @@ class Home extends Component {
                 'Contact'
               ])
             ]),
-            h('a.home-nav-link', { href: `https://twitter.com/${TWITTER_ACCOUNT_NAME}` }, 'Twitter'),
-            h('a.home-nav-link', { href: 'https://github.com/PathwayCommons/factoid' }, 'GitHub')
+            h('a.home-nav-link', { href: `https://twitter.com/${TWITTER_ACCOUNT_NAME}`, target: '_blank' }, 'Twitter'),
+            h('a.home-nav-link', { href: 'https://github.com/PathwayCommons/factoid', target: '_blank' }, 'GitHub')
           ]),
           h('div.home-credit-logos', [
             h('i.home-credit-logo'),

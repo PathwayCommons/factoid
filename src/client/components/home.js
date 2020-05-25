5e876b75f8bc93c88a1665fce30b4e02768803ab
Jeffrey Wong <jvwong@gmail.com>
2020-05-25 11:18:10 -0400

    - Removed unused code for 'context' declaration (signup, journal) - Updated CTA to indicate rationale for inputs

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -6,7 +6,7 @@ import { makeClassList, tryPromise } from '../../util';
 import EventEmitter from 'eventemitter3';
 import { truncateString } from '../../util';
 
-import { EMAIL_CONTEXT_SIGNUP, TWITTER_ACCOUNT_NAME, EMAIL_CONTEXT_JOURNAL, DOI_LINK_BASE_URL } from '../../config';
+import { EMAIL_CONTEXT_SIGNUP, TWITTER_ACCOUNT_NAME, DOI_LINK_BASE_URL } from '../../config';
 
 const checkStatus = response => {
   if ( response.status >= 200 && response.status < 300 ) {
@@ -94,7 +94,7 @@ class RequestForm extends Component {
       };
 
       this.setState({ submitting: true, errors: { incompleteForm: false, network: false } });
-      
+
       ( fetch( url, fetchOpts )
         .then( checkStatus )
         .then( response => response.json() )
@@ -120,35 +120,13 @@ class RequestForm extends Component {
           h( 'a.home-request-form-done-button', { href: privateUrl, target: '_blank', }, 'Start Biofactoid' ),
           h( 'div.home-request-form-done-body', [
             h( doi ? 'a.plain-link': 'span', (doi ? { href: `${DOI_LINK_BASE_URL}${doi}`, target: '_blank'}: {}), `Article: ${articleString}` )
-          ]),
-          h( 'div.home-request-form-done-footer', `An email has also been sent, which includes your article's private editing link.` )
+          ])
         ])
       ]);
     }
 
-    const contextSelector = contexts => {
-      let radios = [];
-      let addType = (typeVal, displayName) => {
-        radios.push(
-          h('input', {
-            type: 'radio',
-            name: `home-request-form-context-${typeVal}`,
-            id: `home-request-form-radio-context-${typeVal}`,
-            value: typeVal,
-            checked: this.state.context === typeVal,
-            onChange: e => this.handleContextChange(e)
-          }),
-          h('label', {
-            htmlFor: `home-request-form-radio-context-${EMAIL_CONTEXT_SIGNUP}`
-          }, displayName)
-        );
-      };
-      contexts.forEach( context => addType( context, _.capitalize( context ) ) );
-      return h( 'div.radioset.home-request-form-radioset', radios );
-    };
-
     return h('div.home-request-form-container', [
-      h('div.home-request-form-description', `Your article's digital pathway`),
+      h('div.home-request-form-description', `Link your article to pathway data`),
       h('i.icon.icon-spinner.home-request-spinner', {
         className: makeClassList({ 'home-request-spinner-shown': this.state.submitting })
       }),
@@ -170,7 +148,7 @@ class RequestForm extends Component {
           value: this.state.authorEmail,
           spellCheck: false
         }),
-        this.props.apiKey ? contextSelector([ EMAIL_CONTEXT_SIGNUP, EMAIL_CONTEXT_JOURNAL ]) : null,
+        h( 'div.home-request-form-footer', `Your email is used to send your article's editing link. It is never shared.` ),
         h('div.home-request-error', {
           className: makeClassList({ 'home-request-error-message-shown': this.state.errors.incompleteForm })
         }, 'Fill out everything above, then try again.'),
@@ -206,7 +184,7 @@ class Scroller extends Component {
 
       this.clearScrollStateDebounced();
     };
-    
+
     this.clearScrollStateDebounced = _.debounce(() => {
       this.setState({ isScrolling: false });
     }, 250);
@@ -391,8 +369,7 @@ class Home extends Component {
       return h(Popover, {
         tippy: {
           html: h(RequestForm, {
-            bus: this.bus,
-            doneMsg: 'Thank you for your request!  We will contact you soon with next steps.'
+            bus: this.bus
           }),
           onHidden: () => this.bus.emit('closecta'),
           placement: 'top'

4afec22bd4ee8cc98f6f82e59f0b2099e2fa0345
Jeffrey Wong <jvwong@gmail.com>
2020-05-27 15:47:45 -0400

    Drop the fetchPubmed debugging code

--- src/server/routes/api/document/index.js
+++ src/server/routes/api/document/index.js
@@ -40,7 +40,7 @@ import { BASE_URL,
   DOCUMENT_IMAGE_CACHE_SIZE,
   EMAIL_TYPE_FOLLOWUP
  } from '../../../../config';
-import { fetchPubmed } from './pubmed/fetchPubmed';
+
 import { ENTITY_TYPE } from '../../../../model/element/entity-type';
 const DOCUMENT_STATUS_FIELDS = Document.statusFields();
 
@@ -255,12 +255,6 @@ let getSbgnFromTemplates = templates => {
   } )
     .then(handleResponseError);
 };
-
-http.get('/fetchPubmed/', function( req, res, next ){
-  fetchPubmed( { uids: req.query.uids} )
-    .then( r => res.json( r ))
-    .catch( next );
-});
 
 /**
  * @swagger

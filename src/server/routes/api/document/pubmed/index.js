d8436abdf2d59323a4fe526261a281b97d9c8a48
Jeffrey Wong <jvwong@gmail.com>
2020-08-10 15:20:48 -0400

    Search PubMed with user's paperId then look through those (top 10) corresponding articles for a match.

--- src/server/routes/api/document/pubmed/index.js
+++ src/server/routes/api/document/pubmed/index.js
@@ -14,21 +14,11 @@ const ID_TYPE = Object.freeze({
   TITLE: 'title'
 });
 
-// 99.3% of CrossRef DOIs (https://www.crossref.org/blog/dois-and-matching-regular-expressions/)
-const doiRegex = /^10\.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
-const digitsRegex = /^[0-9.]+$/;
+const paperId2Type = paperId => {
+  // 99.3% of CrossRef DOIs (https://www.crossref.org/blog/dois-and-matching-regular-expressions/)
+  const doiRegex = /^10\.\d{4,9}\/[-._;()/:A-Z0-9]+$/i;
+  const digitsRegex = /^[0-9.]+$/;
 
-const getUniqueArticleOrThrow = async ( query, IdType ) => {
-  const queryOpts = IdType === ID_TYPE.TITLE ? { field: ID_TYPE.TITLE } : {};
-  const { searchHits, count } = await searchPubmed( query, queryOpts );
-  if( count === 1 ){
-    return _.first( searchHits );
-  } else {
-    throw new ArticleIDError( `Unrecognized paperId '${query}'`, query );
-  }
-};
-
-const findPubmedId = async paperId => {
   let IdType = ID_TYPE.TITLE;
   const isUidLike = digitsRegex.test( paperId );
   const isDoiLike = doiRegex.test( paperId );
@@ -38,8 +28,43 @@ const findPubmedId = async paperId => {
   } else if ( isUidLike ){
     IdType = ID_TYPE.PMID;
   }
-  const foundId = await getUniqueArticleOrThrow( paperId, IdType );
-  return { IdType, foundId };
+
+  return IdType;
+};
+
+const findMatchingPubmedArticle = async ( paperId, IdType, uids ) => {
+  let PubmedArticle;
+  // Pubmed EFETCH returns lower-cased title, adds trailing period
+  const santitize = raw => {
+    const trimmed = _.trim( raw , ' .');
+    const lower = _.toLower( trimmed );
+    const clean = lower.replace(/[\W_]+/g, ' ');
+    return clean;
+  };
+
+  // Try to match: Could be responsibility of user in future
+  const articleMatchesPaperId = article => {
+    const { title, pmid, doi } = getPubmedCitation( article );
+    let hasMatch = false;
+    switch ( IdType ) {
+      case ID_TYPE.DOI:
+        if( doi === paperId ) hasMatch = true;
+        break;
+      case ID_TYPE.PMID:
+        if( pmid === paperId ) hasMatch = true;
+        break;
+      case ID_TYPE.TITLE:
+        if( santitize( title ).includes( santitize( paperId ) ) ) hasMatch = true;
+        break;
+    }
+    return hasMatch;
+  };
+
+  if( uids.length ){
+    const { PubmedArticleSet } = await fetchPubmed({ uids });
+    PubmedArticle = PubmedArticleSet.find( articleMatchesPaperId );
+  }
+  return PubmedArticle;
 };
 
 /**
@@ -59,45 +84,20 @@ const getPubmedArticle = async paperId => {
 
   } else {
     try {
-      const { IdType, foundId } = await findPubmedId( paperId );
-      const { PubmedArticleSet } = await fetchPubmed({
-        uids: [ foundId ]
-      });
+      const IdType = paperId2Type( paperId );
+      const fieldOpts = IdType === ID_TYPE.TITLE ? { field: ID_TYPE.TITLE } : {};
+      const { searchHits } = await searchPubmed( paperId, fieldOpts );
+      const PubmedArticle = await findMatchingPubmedArticle( paperId, IdType, searchHits );
 
-      let hasMatch = false;
-      const PubmedArticle = _.head( PubmedArticleSet );
-      const { title, pmid, doi } = getPubmedCitation( PubmedArticle );
-      // Pubmed EFETCH returns lower-cased title, adds trailing period
-      const santitize = raw => {
-        const trimmed = _.trim( raw , ' .');
-        const lower = _.toLower( trimmed );
-        const clean = lower.replace(/[\W_]+/g, ' ');
-        return clean;
-      };
-
-      switch ( IdType ) {
-        case ID_TYPE.DOI:
-          if( doi === paperId ) hasMatch = true;
-          break;
-        case ID_TYPE.PMID:
-          if( pmid === paperId ) hasMatch = true;
-          break;
-        case ID_TYPE.TITLE:
-          if( santitize( title ).includes( santitize( paperId ) ) ) hasMatch = true;
-          break;
-      }
-
-      if( hasMatch ){
-        return _.head( PubmedArticleSet );
+      if( PubmedArticle ){
+        return PubmedArticle;
       } else {
-        throw new ArticleIDError( `No PubMed record found '${paperId}'`, paperId );
+        throw new Error(`Unable to retrieve a PubmedArticle for '${paperId}`);
       }
 
-    } catch ( e ) {
-      // Error types to note: ArticleIDError, HTTPStatusError, FetchError
-      logger.error( `Unable to retrieve a PubmedArticle for '${paperId}'` );
-      logger.error( `${e.name}: ${e.message}` );
-      throw e;
+    } catch( err ) {
+      logger.error( `${err.name}: ${err.message}` );
+      throw new ArticleIDError( `Unrecognized paperId '${paperId}'`, paperId );
     }
   }
 };

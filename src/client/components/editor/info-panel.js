52086e53df7d8398e47a889ec491c1c96fc2da9d
metincansiper <metincansiper@gmail.com>
2020-11-25 17:02:15 -0800

    Automatic connection of researcher profiles #907

--- src/client/components/editor/info-panel.js
+++ src/client/components/editor/info-panel.js
@@ -1,5 +1,7 @@
 import h from 'react-hyperscript';
 import { Component } from 'react';
+import * as xmljs from 'xml-js';
+import _ from 'lodash';
 import { DOI_LINK_BASE_URL, PUBMED_LINK_BASE_URL, GOOGLE_SCHOLAR_BASE_URL } from '../../../config';
 // import { Carousel, CAROUSEL_CONTENT } from '../carousel';
 import { makeClassList } from '../../dom';
@@ -10,7 +12,45 @@ export class InfoPanel extends Component {
   constructor(props){
     super(props);
 
-    this.state = {};
+    this.state = { orcidMap: null };
+  }
+
+  loadOrcidUriMap(){
+    const { document } = this.props;
+
+    const citation = document.citation();
+    const { authors, doi } = citation;
+
+    const findOrcidUri = ( firstName, lastName ) => {
+      let url = `https://pub.orcid.org/v3.0/search?q=`
+          + `family-name:${lastName}+AND+given-names:${firstName}+AND+digital-object-ids:%22${doi}%22`;
+
+      const findUri = obj => {
+        let els = _.get( obj, [ 'elements', 0, 'elements', 0, 'elements', 0, 'elements' ] );
+        let uriObj = _.find( els, el => el.name = 'common:uri' );
+        let uri = _.get( uriObj, [ 'elements', 0, 'text' ] );
+
+        return uri;
+      };
+
+      return fetch( url )
+        .then( res => res.text() )
+        .then( res => xmljs.xml2json(res) )
+        .then( res => JSON.parse( res ) )
+        .then( findUri );
+    };
+
+    let orcidMap = {};
+    let promises = authors.authorList.map(a => {
+      return findOrcidUri( a.ForeName, a.LastName )
+        .then( orcidUri => {
+          if ( orcidUri ) {
+              orcidMap[ a.name ] = orcidUri;
+          }
+        } );
+    });
+
+    return Promise.all( promises ).then( () => this.setState({ orcidMap }) );
   }
 
   componentDidMount(){
@@ -26,6 +66,8 @@ export class InfoPanel extends Component {
 
     bus.on('select', this.onSelect);
     bus.on('unselect', this.onUnselect);
+
+    this.loadOrcidUriMap();
   }
 
   componentWillUnmount(){
@@ -37,7 +79,7 @@ export class InfoPanel extends Component {
 
   render(){
     const { document, bus, controller } = this.props;
-    const { selected } = this.state;
+    const { selected, orcidMap } = this.state;
 
     if( document.editable() ){ return null; }
 
@@ -66,7 +108,17 @@ export class InfoPanel extends Component {
 
     return h('div.editor-info-panel', [
       h('div.editor-info-title', title),
-      h('div.editor-info-authors', authors.authorList.map(a => h('span.editor-info-author', a.name))),
+      h('div.editor-info-authors', authors.authorList.map(a => {
+        if ( orcidMap == null ) {
+          return h('i.icon.icon-spinner');
+        }
+        let orcidUri = orcidMap[ a.name ];
+        if ( orcidUri ) {
+            return h('a.editor-info-author.plain-link', { target: '_blank', href: orcidUri }, a.name);
+        }
+
+        return h('a.editor-info-author', a.name);
+      })),
       h('div.editor-info-links', doi ? [
         h('a.editor-info-link.plain-link', { target: '_blank', href: `${DOI_LINK_BASE_URL}${doi}` }, reference),
         h('a.editor-info-link.plain-link', { target: '_blank', href: `${PUBMED_LINK_BASE_URL}${pmid}` }, 'PubMed'),

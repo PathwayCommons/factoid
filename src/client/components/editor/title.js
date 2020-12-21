39461a531185724686dc23766cf37b97a65bc159
Max Franz <maxkfranz@gmail.com>
2020-12-21 11:19:09 -0500

    Improved in-editor title editing
    - Title editing is available only if unspecified at doc creation.
    - In place of the title is a link: "Set your article's title".
    - If only the title is missing (i.e. email and name are set), then only the title is available for editing.  This simplifies the workflow for the email campaign.
    - If the name or email fields are empty, then those fields are also available for editing.  The title then says "Set your article's title etc."
    - The submission popovers close properly with the 'Show me how' buttons
    - The form fields are now in the more traditional order: title, name, email

--- src/client/components/editor/title.js
+++ src/client/components/editor/title.js
@@ -1,5 +1,6 @@
 import h from 'react-hyperscript';
 import { Component } from 'react';
+import _ from 'lodash';
 
 import Popover from '../popover/popover';
 import RequestForm from '../request-form';
@@ -33,25 +34,39 @@ class EditorTitle extends Component {
     const { document } = this.props;
     const { citation } = this.state;
     const { authors: { abbreviation }, title = 'Unnamed document', reference, doi } = citation;
+    const provided = document.provided() || {};
 
     if( !document.editable() ){
       return null;
     }
 
+    const otherMissingFields = !provided.authorName || !provided.authorEmail;
+
     const getTitleContent = () => {
        if ( title == null ) {
          return h( Popover, {
+           show: showTippy => {
+             this.bus.on('showedittitle', showTippy); // for submit validation: show me how
+           },
            tippy: {
              html: h( RequestForm, {
                doc: document,
                bus: this.bus,
-               submitBtnText: 'Okay'
+               submitBtnText: 'OK',
+               showDescription: false,
+               showTitle: false,
+               addClasses: '.editor-request-form-container',
+               formFields: {
+                 authorName: _.get(document.provided(), ['authorName']),
+                 authorEmail: _.get(document.provided(), ['authorEmail'])
+               }
              }),
+             onShown: () => this.bus.emit('opencta'),
              onHidden: () => this.bus.emit( 'closecta' ),
              placement: 'top'
            }
          }, [
-           h( 'span.plain-link.link-like', 'Set the article\'s title' )
+           h( 'span.plain-link.link-like', `Set your article's title` + (otherMissingFields ? ' etc.' : '') )
          ]);
        }
 

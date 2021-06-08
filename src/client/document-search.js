5ed0f462f39e34200aca7d786b99d2f41fbd5337
Jeffrey Wong <jvwong@gmail.com>
2021-06-08 15:25:18 -0400

    - return the full (raw) indexed documents in search hits - make the entire list of documents available

--- src/client/document-search.js
+++ src/client/document-search.js
@@ -1,30 +1,45 @@
 import MiniSearch from 'minisearch';
 import _ from 'lodash';
+import querystring from 'querystring';
+
+import logger from './logger';
 
 /** Class for  searching an index. */
 class Search {
 
   constructor( opts = {} ){
-    this._engine = new MiniSearch( opts );
+    this._engine = new MiniSearch( _.assign( {}, opts, { storeFields: [ 'id' ] } ) );
+    this._documents = [];
+    this._docMap = new Map();
   }
 
   /**
-   * Load documents into the index
-   * @param {object} documents List of Documents to index
+   * Load documents into the index and stash locally
+   * @param {object} documents List of raw documents to index
    */
   index( documents ){
     this._engine.addAll( documents );
+    this._documents = documents;
+    this._documents.forEach( doc => this._docMap.set( doc.id, doc ) );
+  }
+
+  /**
+   * Getter for list of raw documents indexed
+   */
+  get documents() {
+    return this._documents;
   }
 
   /**
    * Search the index
    * @param {string} q search query
    * @param {number} limit maximum number of hits to return (default 10)
-   * @return {object} a list of search hits
+   * @return {object} a list of search hits (raw document)
    */
-   search( q, limit = 10 ){
-    const searchHits = this._engine.search( q );
-    return searchHits.slice( 0, limit );
+  search( q, limit = 10 ){
+    let searchHits = this._engine.search( q );
+    searchHits = searchHits.slice( 0, limit );
+    return searchHits.map( ({ id }) => this._docMap.get( id ) );
   }
 }
 
@@ -69,18 +84,33 @@ class DocumentSearch extends Search {
         'citation.reference',
         'citation.abstract',
         'entities'
-      ],
-
-      // fields to be stored and returned as part of the search results.
-      storeFields: [
-        'id',
-        'citation'
       ]
     };
 
     const docOpts = _.defaults( opts, DEFAULT_DOCUMENT_OPTS );
 
     super( docOpts );
+  }
+
+  /**
+   * Fetch documents from source and index
+   * @param {string} url location to retrieve documents from
+   * @param {number} limit max documents to retrieve
+   * @param {number} offset starting index for documents to retrieve
+   * @return {object} a list of full Document JSON
+   */
+  fetch( url, limit = 50, offset = 0 ){
+    const opts = { limit, offset };
+    let addr = `${url}?${querystring.stringify( opts )}`;
+
+    return fetch( addr )
+      .then( res => res.json() )
+      .then( docs => this.index( docs ) )
+      .then( () => this.documents )
+      .catch( err => {
+        logger.error( `Unable to load documents from ${url}` );
+        logger.error( err );
+      });
   }
 }
 

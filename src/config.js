29b5086a304a55daa250e4a8cf9b770e929a9515
Jeffrey Wong <jvwong@gmail.com>
2023-02-03 15:46:55 -0500

    Use Google Tag Manager instead of GA Tag.

--- src/config.js
+++ src/config.js
@@ -136,4 +136,5 @@ export const SEMANTIC_SEARCH_LIMIT = env('SEMANTIC_SEARCH_LIMIT', 30);
 export const MIN_RELATED_PAPERS = env('MIN_RELATED_PAPERS', 6);
 
 // google analytics
-export const GA_ID = env('GA_ID', 'G-15EGSCBK77');
+// google tag manager
+export const GTM_ID = env('GTM_ID', 'GTM-NV468LC');

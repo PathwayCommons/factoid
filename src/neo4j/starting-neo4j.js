b6ba65bd5440fffaebed9b32308233bf1034c18c
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-02-09 14:33:30 -0500

    put document functions in a different file for organization

--- src/neo4j/starting-neo4j.js
+++ src/neo4j/starting-neo4j.js
@@ -1,36 +1,6 @@
 // const neo4j = require('neo4j-driver');   <-- old, read up on imports and exports in book
 import neo4j from 'neo4j-driver';
 
-async function loadDoc(data, docSocket, eleSocket) {
-    const { id, secret } = data;
-
-    const doc = new Document({
-        socket: docSocket,
-        factoryOptions: { socket: eleSocket },
-        data: { id, secret }
-    });
-
-    await doc.load();
-    await doc.sync(true);
-
-    return doc;
-}
-
-async function toDocs(docJSONs, docSocket, eleSocket) {
-    const docPromises = docJSONs.map(async docJSON => await loadDoc(docJSON, docSocket, eleSocket));
-
-    return Promise.all(docPromises);
-}
-
-async function getAllDocs() {// eslint-disable-line no-unused-vars
-    const url = 'https://biofactoid.org/api/document/a896d611-affe-4b45-a5e1-9bc560ffceab';
-
-    const res = await fetch(`${url}`);
-    const docJSONs = await res.json();
-    return await toDocs(docJSONs, this.docSocket, this.eleSocket);
-}
-
-
 const driver = neo4j.driver('bolt://localhost:7687');
 
 // Format of nodeData are as follows:

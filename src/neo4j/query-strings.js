c0a491ffca9eb33e95eb43b39e132e8cefcece15
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-02-10 16:06:53 -0500

    put query strings and hard coded data in different files

---
+++ src/neo4j/query-strings.js
@@ -0,0 +1,18 @@
+export const makeNodeQuery =
+    `MERGE (gene:Gene {id: $id})
+    ON CREATE SET gene.factoidId = $factoidId, 
+    gene.name = $name,
+    gene.type = $type,
+    gene.dbId = $dbId,
+    gene.dbName = $dbName
+    RETURN gene.name`;
+
+export const makeRelationshipQuery =
+    `MATCH (x:Gene {id: $id1})
+    MATCH (y:Gene {id: $id2})
+    MERGE (x)-[r:INTERACTION {id: $id3}]->(y)
+    ON CREATE SET r.type = $type,
+    r.doi = $doi, 
+    r.pmid = $pmid,
+    r.documentId = $documentId,
+    r.title = $title`;
--- src/neo4j/starting-neo4j.js
+++ src/neo4j/starting-neo4j.js
@@ -1,35 +1,10 @@
-// const neo4j = require('neo4j-driver');   <-- old, read up on imports and exports in book
 import neo4j from 'neo4j-driver';
+import { makeNodeQuery, makeRelationshipQuery } from './query-strings';
+import { nodeData, relationshipData } from './graph-data';
 
 const driver = neo4j.driver('bolt://localhost:7687');
 
-// Format of nodeData are as follows:
-// { id: 'element.association.dbPrefix'+ ':' + 'element.association.id', factoidId: 'element.id',
-// name: 'element.name', type: 'element.type', dbId: 'element.association.id',
-// dbName: "element.association.dbName"}
-const nodeData = [
-    // MAPK6 data
-    {
-        id: 'ncbigene:5597', factoidId: '598f8bef-f858-4dd0-b1c6-5168a8ae5349', name: 'MAPK6',
-        type: 'protein', dbId: '5597', dbName: 'NCBI Gene'
-    },
-    // AKT data
-    {
-        id: 'ncbigene:207', factoidId: '4081348e-20b8-4bf8-836f-695827a4f9a2', name: 'AKT',
-        type: 'protein', dbId: '207', dbName: 'NCBI Gene'
-    }
-];
-
-const makeNodeQuery =
-    `MERGE (gene:Gene {id: $id})
-    ON CREATE SET gene.factoidId = $factoidId, 
-    gene.name = $name,
-    gene.type = $type,
-    gene.dbId = $dbId,
-    gene.dbName = $dbName
-    RETURN gene.name`;
-
-// this test makes the MAPK6 node
+// this test makes the MAPK6 node. Successful
 export async function makeMAPK6Test() {
     let session;
     try {
@@ -40,13 +15,13 @@ export async function makeMAPK6Test() {
         console.error(error);
         throw error;
     } finally {
-    session.close();
-    driver.close();
+        session.close();
+        driver.close();
     }
+    return;
 }
 
-// this test does as I want (make both nodes at once) but it doesn't 
-//   exit by itself (need to use Ctrl-C)
+// this test does as I want (make both nodes at once) but it doesn't exit by itself (need to use Ctrl-C)
 export async function makeGeneNodeTest() {
     //let session;
     for (let i = 0; i < nodeData.length; i++) {
@@ -54,7 +29,7 @@ export async function makeGeneNodeTest() {
         try {
             session = driver.session({ database: "neo4j" });
             let result = await session.run(makeNodeQuery, nodeData[i]);
-            console.log("Gene Node created");
+            console.log("Gene Node created: ", result.records);
             session.close();
         } catch (error) {
             console.error(error);
@@ -63,31 +38,12 @@ export async function makeGeneNodeTest() {
             throw error;
         }
     }
+    console.log("Loop ends!");
+    return;
 }
 
-// Parameters of relationshipData are as follows:
-// { id1: 'element.association.dbPrefix'+ ':' + 'element.association.id',
-// id2: 'element.association.dbPrefix'+ ':' + 'element.association.id', id3: 'element.id',
-// type: 'element.type', doi: 'value.citation.doi',
-// pmid: 'value.citation.pmid', documentId: 'value.id', title: 'value.citation.title'}
-const relationshipData = [{
-    id1: 'ncbigene:5597', id2: 'ncbigene:207', id3: '01ef22cc-2a8e-46d4-9060-6bf1c273869b',
-    type: 'phosphorylation', doi: '10.1126/sciadv.abi6439', pmid: '34767444',
-    documentId: 'a896d611-affe-4b45-a5e1-9bc560ffceab',
-    title: 'MAPK6-AKT signaling promotes tumor growth and resistance to mTOR kinase blockade.'
-}];
-
-const makeRelationshipQuery =
-    `MATCH (x:Gene {id: $id1})
-    MATCH (y:Gene {id: $id2})
-    MERGE (x)-[r:INTERACTION {id: $id3}]->(y)
-    ON CREATE SET r.type = $type,
-    r.doi = $doi, 
-    r.pmid = $pmid,
-    r.documentId = $documentId,
-    r.title = $title`;
-
-export async function test() { // This test does not yet work!!!
+// This test does not yet work!!!
+export async function test() { 
     const session = driver.session({ database: "neo4j" });
     await session
         .beginTransaction()

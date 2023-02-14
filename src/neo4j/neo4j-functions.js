c2e484e099f90263c016703961d639262ea766b6
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-02-14 10:40:44 -0500

    Made addNode, addEdge. addNode works confirmed

---
+++ src/neo4j/neo4j-functions.js
@@ -0,0 +1,60 @@
+import neo4j from 'neo4j-driver';
+import { giveInfoByGeneId, makeNodeQuery, makeRelationshipQuery } from './query-strings';
+
+export async function addNode(params) {
+    const driver = neo4j.driver('bolt://localhost:7687');
+    let session;
+    try {
+        session = driver.session({ database: "neo4j" });
+        let result = await session.executeWrite(tx => {
+            return tx.run(makeNodeQuery, params);
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        await session.close();
+        driver.close();
+    }
+    return;
+}
+
+export async function addEdge(params) {
+    const driver = neo4j.driver('bolt://localhost:7687');
+    let session;
+    try {
+        session = driver.session({ database: "neo4j" });
+        let result = await session.executeWrite(tx => {
+            return tx.run(makeRelationshipQuery, params);
+        });
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        await session.close();
+        driver.close();
+    }
+    return;
+}
+
+export async function searchByGeneId(id) {
+    const driver = neo4j.driver('bolt://localhost:7687');
+    let session;
+    try {
+        session = driver.session({ database: "neo4j" });
+        let result = await session.executeRead(tx => {
+            return tx.run(giveInfoByGeneId, { id: id });
+        });
+        let names = result.records.map(row => {
+            return row.get('m');
+        });
+        return names;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        await session.close();
+        driver.close();
+    }
+    return;
+}
--- src/neo4j/test-temp-run.js
+++ src/neo4j/test-temp-run.js
@@ -1,6 +1,7 @@
 import { read1, test, makeMAPK6Test, makeGeneNodeTest } from './starting-neo4j.js';
+import { addNode, addEdge, searchByGeneId } from './neo4j-functions';
 
-async function makeNodeTest() {
+async function makeNodeTest() { // eslint-disable-line no-unused-vars
     console.log("Starting makeNodeTest");
     //await makeMAPK6Test();
     //await makeGeneNodeTest();
@@ -9,4 +10,23 @@ async function makeNodeTest() {
     console.log("Finish makeNodeTest");
 }
 
-makeNodeTest();
+async function testingAddFunctions() {
+    console.log("Begin test");
+    // Add MAPK6 Node
+    await addNode({
+        id: 'ncbigene:5597', factoidId: '598f8bef-f858-4dd0-b1c6-5168a8ae5349', name: 'MAPK6',
+        type: 'protein', dbId: '5597', dbName: 'NCBI Gene'
+    })
+
+    // Add AKT Node
+    await addNode({
+        id: 'ncbigene:207', factoidId: '4081348e-20b8-4bf8-836f-695827a4f9a2', name: 'AKT',
+        type: 'protein', dbId: '207', dbName: 'NCBI Gene'
+    })
+
+    // Add Interaction
+    
+    console.log("End test");
+}
+
+testingAddFunctions();

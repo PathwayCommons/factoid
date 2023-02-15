4d36f408f7d1c7d5f9af64a5365a15e61155b2ef
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-02-15 17:16:20 -0500

    made first test (not yet run successfully)

---
+++ test/neo4j/connect-neo4j.js
@@ -0,0 +1,37 @@
+//import { config } from 'dotenv';
+import { expect } from 'chai';
+import { closeDriver, getDriver, initDriver } from '../../src/neo4j/neo4j-driver.js';
+
+describe('01. Initiate Driver', () => {
+    //beforeAll(() => config());
+    //afterAll(() => closeDriver());
+
+    it('Should create a driver instance and connect to server', async () => {
+        await initDriver();
+    });
+
+    it('Driver has been instantiated', () => {
+        const driver = getDriver();
+        expect(driver).toBeDefined();
+
+        expect(driver.constructor.name).toEqual('Driver');
+    });
+
+    it('Driver can verify connectivity', () => {
+        const driver = getDriver();
+        expect(driver).toBeDefined();
+        expect(driver.constructor.name).toEqual('Driver');
+
+        driver.verifyConnectivity()
+            .then(() => {
+                expect(true).toEqual(true);
+            })
+            .catch(e => {
+                expect(e).toBeUndefined('Unable to verify connectivity');
+            });
+    });
+
+    it('Close the Driver', () => {
+        closeDriver();
+    });
+});

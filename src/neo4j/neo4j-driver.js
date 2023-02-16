b00479def6b5b82ea13478da363eb5b4f427c1e5
Jeffrey Wong <jvwong@gmail.com>
2023-02-16 17:39:14 -0500

    Configuration for graph db.

--- src/neo4j/neo4j-driver.js
+++ src/neo4j/neo4j-driver.js
@@ -1,17 +1,35 @@
 import neo4j from 'neo4j-driver';
+import { GRAPHDB_CONN, GRAPHDB_USER, GRAPHDB_PASS } from '../config';
 
 let driver;
 
-export function initDriver() {
-    driver = neo4j.driver('bolt://localhost:7687');
-    return driver.verifyConnectivity()
-    .then( () => driver);
+/**
+ * Initialize the Neo4j driver singleton
+ *
+ * @returns Neo4j driver instance
+ */
+export async function initDriver() {
+  driver = neo4j.driver( GRAPHDB_CONN,
+    neo4j.auth.basic( GRAPHDB_USER, GRAPHDB_PASS )
+  );
+  await driver.verifyConnectivity();
+  return driver;
 }
 
+/**
+ * Retrieve the Neo4j driver instance
+ *
+ * @returns Neo4j driver instance, possibly undefined
+ */
 export function getDriver() {
-    return driver;
+  return driver;
 }
 
-export function closeDriver() {
-    return driver && driver.close();
+/**
+ * Close the Neo4j driver instance
+ *
+ * @returns Promise<void>
+ */
+export async function closeDriver() {
+  return driver && driver.close();
 }

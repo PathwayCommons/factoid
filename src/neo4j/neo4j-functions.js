fee908d2b36ec75387f9316e0760b7d144bb5c9a
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-02-24 12:03:54 -0500

    documentation

--- src/neo4j/neo4j-functions.js
+++ src/neo4j/neo4j-functions.js
@@ -1,7 +1,11 @@
-import neo4j from 'neo4j-driver';
-import { giveConnectedInfoByGeneId, makeNodeQuery, makeRelationshipQuery } from './query-strings';
+import { giveConnectedInfoByGeneId, makeNodeQuery, makeRelationshipQuery, returnGeneById } from './query-strings';
 import { getDriver } from './neo4j-driver';
 
+/**
+ * @param { String } id in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @param { String } name 
+ * @returns 
+ */
 export async function addNode(id, name) {
     const driver = getDriver();
     let session;
@@ -23,6 +27,17 @@ export async function addNode(id, name) {
     return;
 }
 
+/**
+ * @param { String } id interaction element's UUID (NOT document id)
+ * @param { String } type 
+ * @param { String } sourceId in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @param { String } targetId in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @param { String } xref document UUID
+ * @param { String } doi 
+ * @param { String } pmid 
+ * @param { String } articleTitle 
+ * @returns 
+ */
 export async function addEdge(id, type, sourceId, targetId, xref, doi, pmid, articleTitle) {
     const driver = getDriver();
     let session;
@@ -50,6 +65,10 @@ export async function addEdge(id, type, sourceId, targetId, xref, doi, pmid, art
     return;
 }
 
+/**
+ * @param { String } id in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @returns An object with 2 fields: relationships (array) and neighbouring nodes (array) or null
+ */
 export async function searchByGeneId(id) {
     const driver = getDriver();
     let session;
@@ -73,6 +92,10 @@ export async function searchByGeneId(id) {
     return record;
 }
 
+/**
+ * @param { String } id in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @returns an array of nodes that are neighbours to the specified gene
+ */
 export async function getNeighbouringNodes(id) {
     let record = await searchByGeneId(id);
     if (record.length == 0) {
@@ -83,6 +106,10 @@ export async function getNeighbouringNodes(id) {
     });
 }
 
+/**
+ * @param {*} id in the form of "dbName:dbId", ex: "ncbigene:207"
+ * @returns an array of relationships leading away from/leading to the specified gene
+ */
 export async function getInteractions(id) {
     let record = await searchByGeneId(id);
     if (record.length == 0) {

5a596437cb28b5211bc407f29c4e545e9e082b42
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-03-03 15:08:47 -0500

    citation field is not populating (fields are null)

--- neo4j-test/doc-tests.js
+++ neo4j-test/doc-tests.js
@@ -4,11 +4,14 @@ import testDoc from './testDoc.json';
 import uuid from 'uuid';
 import { loadTables, createDoc, deleteDoc } from '../src/neo4j/get-doc-functions.js';
 
-describe('Tests for addNode, addEdge and seachByGeneId', function () {
+// Note: Before running, make sure to delete entries in database
+//        and restart everything (npm run watch)
+
+describe('Tests for Documents', function () {
 
   let dummyDoc;
 
-  before('Should create a dummy doc', async function () {
+  before('Create a dummy doc', async function () {
     const { docDb, eleDb } = await loadTables();
     const id = uuid(); //npm uuid
     const secret = uuid(); // npm uuid
@@ -26,9 +29,39 @@ describe('Tests for addNode, addEdge and seachByGeneId', function () {
     await deleteDoc(dummyDoc);
   });
 
-  it('Blah blah', async function () {
+  it('Make sure dummy doc has been made with necessary fields', async function () {
     const { total, results } = await getDocuments({});
     const myDummyDoc = results.filter(d => d.id == dummyDoc.id());
     expect(myDummyDoc.length).to.equal(1);
+
+    let myDoc = myDummyDoc[0];
+    expect(myDoc.id).to.equal(dummyDoc.id());
+    //expect(myDoc.citation.doi).to.equal('10.1126/sciadv.abi6439'); // Why isn't citation populating?
+    //expect(myDoc.citation.pmid).to.equal('34767444');
+    expect(myDoc.text).to.equal('MAPK6 activates AKT via phosphorylation.');
+
+    expect(myDoc.elements.length).to.equal(3);
+
+    let first = myDoc.elements[0];
+    expect(first.association.dbPrefix).to.equal('ncbigene');
+    expect(first.association.name).to.equal('MAPK6');
+    expect(first.association.id).to.equal('5597');
+
+    let second = myDoc.elements[1];
+    expect(second.association.dbPrefix).to.equal('ncbigene');
+    expect(second.association.name).to.equal('AKT1');
+    expect(second.association.id).to.equal('207');
+
+    let third = myDoc.elements[2];
+    expect(third.id).to.equal('01ef22cc-2a8e-46d4-9060-6bf1c273869b');
+    expect(third.type).to.equal('phosphorylation');
+    expect(third.entries.length).to.equal(2);
+    expect(third.entries[0].id).to.equal('598f8bef-f858-4dd0-b1c6-5168a8ae5349');
+    expect(third.entries[1].id).to.equal('4081348e-20b8-4bf8-836f-695827a4f9a2');
   });
+
+  it('Add the elements of dummy doc to Neo4j db', async function () {
+    expect(true).to.equal(true);
+  });
+
 });

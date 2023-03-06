cdb7440272ac85faf2b2253d728c4c28a2b5f7c0
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-03-06 15:55:23 -0500

    made function convertUUIDtoId, less hard coding

--- src/neo4j/neo4j-document.js
+++ src/neo4j/neo4j-document.js
@@ -2,6 +2,16 @@ import { addNode, addEdge } from './neo4j-functions';
 
 let nodeTypes = ['entity', 'ggp', 'dna', 'rna', 'protein', 'chemical', 'complex'];
 
+function convertUUIDtoId(docElements, id) {
+  for (let i = 0; i < docElements.length; i++) {
+    let e = docElements[i];
+    if (e.id == id) {
+      return `${e.association.dbPrefix}:${e.association.id}`;
+    }
+  }
+  return 'node id not found';
+}
+
 export async function addDocumentToNeo4j(doc) {
 
   // Step 1: Sort each element in a document into one of two categories
@@ -12,32 +22,31 @@ export async function addDocumentToNeo4j(doc) {
   let docElements = doc.elements;
   for (let i = 0; i < docElements.length; i++) {
     let e = docElements[i];
-    console.log(e.type);
     if (nodeTypes.includes(e.type)) {
       let nodeInfo = {
-        id: e.association.dbPrefix + ':' + e.association.id,
+        id: `${e.association.dbPrefix}:${e.association.id}`,
         name: e.association.name
       };
       arrNodes.push(nodeInfo);
     } else {
+      let sourceUUId;
+      let targetUUId;
+      if (e.entries[0].group == null) {
+        sourceUUId = e.entries[0].id;
+        targetUUId = e.entries[1].id;
+      } else {
+        sourceUUId = e.entries[1].id;
+        targetUUId = e.entries[0].id;
+      }
       let edgeInfo = {
         id: e.id,
         type: e.type,
-        sourceId: 'ncbigene:5597',  // To Do
-        targetId: 'ncbigene:207'
+        sourceId: convertUUIDtoId(docElements, sourceUUId),
+        targetId: convertUUIDtoId(docElements, targetUUId)
       };
       arrEdges.push(edgeInfo);
     }
   }
-  /*let arrNodes = [{ id: 'ncbigene:5597', name: 'MAPK6' }, { id: 'ncbigene:207', name: 'AKT' }];
-  let arrEdges =
-    [{
-      id: '01ef22cc-2a8e-46d4-9060-6bf1c273869b', type: 'phosphorylation',
-      sourceId: 'ncbigene:5597', targetId: 'ncbigene:207',
-      xref: 'a896d611-affe-4b45-a5e1-9bc560ffceab', doi: '10.1126/sciadv.abi6439',
-      pmid: '34767444', articleTitle: '"MAPK6-AKT signaling promotes tumor growth and resistance to mTOR kinase blockade."'
-    }];*/
-
 
   let docCitations = {
     xref: doc.id,

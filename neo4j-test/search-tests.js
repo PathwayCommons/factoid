eb796446287dabb1df504e6d9b8e1951c307b9f3
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-03-15 16:08:11 -0400

    add a doi field to tests

--- neo4j-test/search-tests.js
+++ neo4j-test/search-tests.js
@@ -75,9 +75,9 @@ describe('04. Tests for searchByGeneId', function () {
     expect(await searchByGeneId(kank1)).to.not.be.null;
     let edges = await getInteractions(kank1);
     expect(edges.length).equal(3);
-    expect(_.find(edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9' })).to.be.not.undefined;
-    expect(_.find(edges, { id: '5a374667-a51d-4aa3-bf93-526fe203b04e' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '5a374667-a51d-4aa3-bf93-526fe203b04e', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
 
     let nodes = await getNeighbouringNodes(kank1);
     expect(nodes.length).equal(3);
@@ -91,12 +91,12 @@ describe('04. Tests for searchByGeneId', function () {
     expect(await searchByGeneId(tln1)).to.not.be.null;
     let edges = await getInteractions(tln1);
     expect(edges.length).equal(6);
-    expect(_.find(edges, { id: '028e7366-9779-4466-96ea-18a45bfe3f38' })).to.be.not.undefined;
-    expect(_.find(edges, { id: '80a4d403-8bc1-4ddb-a4f9-5227e87ef6fa' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9' })).to.be.not.undefined;
-    expect(_.find(edges, { id: '013b7e2a-7240-4668-b628-2653c60f47e9' })).to.be.not.undefined;
-    expect(_.find(edges, { id: '3e7c85db-c2a3-4a1f-b96e-6d187c6ab93b' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'e39d0a06-5b02-44e3-9c36-27cc1f9ac08c' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '028e7366-9779-4466-96ea-18a45bfe3f38', doi: '10.1016/j.str.2016.04.016' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '80a4d403-8bc1-4ddb-a4f9-5227e87ef6fa', doi: '10.1016/j.str.2016.04.016' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '013b7e2a-7240-4668-b628-2653c60f47e9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '3e7c85db-c2a3-4a1f-b96e-6d187c6ab93b', doi: '10.1016/j.jbc.2021.100837' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'e39d0a06-5b02-44e3-9c36-27cc1f9ac08c', doi: '10.1016/j.jbc.2021.100837' })).to.be.not.undefined;
 
     let nodes = await getNeighbouringNodes(tln1);
     expect(nodes.length).equal(5);
@@ -112,11 +112,11 @@ describe('04. Tests for searchByGeneId', function () {
     expect(await searchByGeneId(tlnrd1)).to.not.be.null;
     let edges = await getInteractions(tlnrd1);
     expect(edges.length).equal(5);
-    expect(_.find(edges, { id: 'd5d9fbcc-a1d9-4026-b1d3-d4a97faff36b' })).to.be.not.undefined;
-    expect(_.find(edges, { id: '13ab91d1-ee5f-46ca-a1ea-82ce72a938f4' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'eec84ebe-eece-4143-b406-cd99cdbe2e43' })).to.be.not.undefined;
-    expect(_.find(edges, { id: 'e59c16c4-11e8-4755-8e2f-f88fe4a73b30' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'd5d9fbcc-a1d9-4026-b1d3-d4a97faff36b', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(edges, { id: '13ab91d1-ee5f-46ca-a1ea-82ce72a938f4', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'eec84ebe-eece-4143-b406-cd99cdbe2e43', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(edges, { id: 'e59c16c4-11e8-4755-8e2f-f88fe4a73b30', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
 
     let nodes = await getNeighbouringNodes(tlnrd1);
     expect(nodes.length).equal(5);

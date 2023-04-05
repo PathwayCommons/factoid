61b6f35bacec2464c21c22383b116d221714216c
Linda (Jia Wen) Li <lindor.2019@gmail.com>
2023-04-05 14:47:37 -0400

    testing for neighbourhoodReadable complete

--- neo4j-test/search-tests.js
+++ neo4j-test/search-tests.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 
 import { loadDoc } from '../src/server/routes/api/document/index.js';
 import { initDriver, closeDriver } from '../src/neo4j/neo4j-driver.js';
-import { neighbourhood, getInteractions, getNeighbouringNodes, get } from '../src/neo4j/neo4j-functions.js';
+import { neighbourhood, neighbourhoodReadable, getInteractions, getNeighbouringNodes, get } from '../src/neo4j/neo4j-functions.js';
 import { addDocumentToNeo4j } from '../src/neo4j/neo4j-document.js';
 import { deleteAllNodesAndEdges } from '../src/neo4j/test-functions.js';
 
@@ -66,6 +66,7 @@ describe('04. Tests for search functions', function () {
   it('Search for MAPK6', async function () {
     let mapk6 = 'ncbigene:5597';
     expect(await neighbourhood(mapk6)).to.be.null;
+    expect(await neighbourhoodReadable(mapk6)).to.be.null;
     expect(await getInteractions(mapk6)).to.be.null;
     expect(await getNeighbouringNodes(mapk6)).to.be.null;
   });
@@ -84,6 +85,17 @@ describe('04. Tests for search functions', function () {
     expect(_.find(nodes, { id: 'ncbigene:59274', name: 'TLNRD1' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:7094', name: 'TLN1' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:83660', name: 'TLN2' })).to.be.not.undefined;
+
+    let record = await neighbourhoodReadable(kank1);
+    expect(record.neighbouringNodes.length).equal(3);
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:59274', name: 'TLNRD1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:7094', name: 'TLN1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:83660', name: 'TLN2' })).to.be.not.undefined;
+
+    expect(record.edges.length).equal(3);
+    expect(_.find(record.edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: '5a374667-a51d-4aa3-bf93-526fe203b04e', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
   });
 
   it('Search for TLN1', async function () {
@@ -105,6 +117,22 @@ describe('04. Tests for search functions', function () {
     expect(_.find(nodes, { id: 'ncbigene:10395', name: 'DLC1' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:23189', name: 'KANK1' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:25959', name: 'KANK2' })).to.be.not.undefined;
+
+    let record = await neighbourhoodReadable(tln1);
+    expect(record.edges.length).equal(6);
+    expect(_.find(record.edges, { id: '028e7366-9779-4466-96ea-18a45bfe3f38', doi: '10.1016/j.str.2016.04.016' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: '80a4d403-8bc1-4ddb-a4f9-5227e87ef6fa', doi: '10.1016/j.str.2016.04.016' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'e56263d8-d5b6-4812-bc2f-8d905a66f0f9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: '013b7e2a-7240-4668-b628-2653c60f47e9', doi: '10.7554/eLife.18124' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: '3e7c85db-c2a3-4a1f-b96e-6d187c6ab93b', doi: '10.1016/j.jbc.2021.100837' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'e39d0a06-5b02-44e3-9c36-27cc1f9ac08c', doi: '10.1016/j.jbc.2021.100837' })).to.be.not.undefined;
+
+    expect(record.neighbouringNodes.length).equal(5);
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:5829', name: 'PXN' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:983', name: 'CDK1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:10395', name: 'DLC1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:23189', name: 'KANK1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:25959', name: 'KANK2' })).to.be.not.undefined;
   });
 
   it('Search for TLNRD1', async function () {
@@ -125,6 +153,21 @@ describe('04. Tests for search functions', function () {
     expect(_.find(nodes, { id: 'ncbigene:60', name: 'ACTB' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:54518', name: 'APBB1IP' })).to.be.not.undefined;
     expect(_.find(nodes, { id: 'ncbigene:65059', name: 'RAPH1' })).to.be.not.undefined;
+
+    let record = await neighbourhoodReadable(tlnrd1);
+    expect(record.edges.length).equal(5);
+    expect(_.find(record.edges, { id: 'd5d9fbcc-a1d9-4026-b1d3-d4a97faff36b', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: '13ab91d1-ee5f-46ca-a1ea-82ce72a938f4', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'd7b2a15d-43bf-4494-815b-a77e08cea59c', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'eec84ebe-eece-4143-b406-cd99cdbe2e43', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+    expect(_.find(record.edges, { id: 'e59c16c4-11e8-4755-8e2f-f88fe4a73b30', doi: '10.1083/jcb.202005214' })).to.be.not.undefined;
+
+    expect(record.neighbouringNodes.length).equal(5);
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:59274', name: 'TLNRD1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:23189', name: 'KANK1' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:60', name: 'ACTB' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:54518', name: 'APBB1IP' })).to.be.not.undefined;
+    expect(_.find(record.neighbouringNodes, { id: 'ncbigene:65059', name: 'RAPH1' })).to.be.not.undefined;
   });
 
   it('Search for document that does not exist', async function () {

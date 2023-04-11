d3d5dbd2b5ea7a7a6a958470b25900349ad57a11
Jeffrey Wong <jvwong@gmail.com>
2023-04-11 16:24:11 -0400

    Implement a refresh function for Graph DB

--- src/server/routes/api/document/graphdb.js
+++ src/server/routes/api/document/graphdb.js
@@ -4,6 +4,10 @@ import logger from '../../../logger';
 import { loadDoc, loadTables } from './index';
 import { initDriver } from '../../../../neo4j/neo4j-driver';
 import { addDocumentToNeo4j } from '../../../../neo4j';
+import Document from '../../../../model/document';
+import { guaranteeSession } from '../../../../neo4j/neo4j-driver';
+
+const DOCUMENT_STATUS_FIELDS = Document.statusFields();
 
 const handleDocChange = async (err, item) => {
 
@@ -76,6 +80,93 @@ const setupGraphDbFeeds = async () => {
 
   const cursor = await docChangefeeds();
   cursor.each( handleDocChange );
+};
+
+
+let mtime = null; // milliseconds
+const lastUpdateTime = t => {
+  if( !t ){
+    return mtime;
+  } else {
+    mtime = t;
+  }
+};
+
+const docsToRefresh = async () => {
+  const tables = await loadTables();
+  const { docDb, eleDb } = tables;
+  let { table: q, conn, rethink: r } = docDb;
+
+  // Filter: Include status 'public'
+  q = q.filter( r.row( 'status' ).eq( DOCUMENT_STATUS_FIELDS.PUBLIC ) );
+  q = q.pluck([ 'id', 'secret' ]);
+
+  const cursor =  await q.run( conn );
+  const dbJSON = await cursor.toArray();
+  return Promise.all( dbJSON.map( ({ id, secret }) => loadDoc({ docDb, eleDb, id, secret }) ));
+};
+
+const populateGraphDb = async () => {
+  logger.debug( `Calling populateGraphDb` );
+
+  try {
+    const docs = await docsToRefresh();
+    for ( const doc of docs ) {
+      await addDocumentToNeo4j( doc );
+    }
+  } catch ( err ) { // swallow
+    logger.error( 'Failed to populate graph DB from factoid' );
+    logger.error( err );
+  }
+};
+
+const clearGraphDb = async () => {
+  logger.debug( `Calling clearGraphDb` );
+  // replace with something like 'deleteAllNodesAndEdges'
+  let session;
+  try {
+    session = guaranteeSession();
+    await session.executeWrite(tx => {
+      return tx.run(`MATCH (n) DETACH DELETE n`);
+    });
+  } catch (error) {
+    throw error;
+  } finally {
+    await session.close();
+  }
+  return;
+};
+
+/**
+ * refresh
+ *
+ * Refresh the Graph DB with factoid data
+ *
+ * @param {Number} refreshPeriodMinutes The time, in minutes, between successive refreshes
+ */
+export const refreshGraphDB = async refreshPeriodMinutes => {
+  const SECONDS_PER_MINUTE = 60;
+  const MILLISECONDS_PER_SECOND = 1000;
+  const minutesToMs = m => m * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND;
+
+  try {
+    let shouldRefresh = false;
+    if( lastUpdateTime() == null ){
+      shouldRefresh = true;
+    } else {
+      const timeSinceLastUpdate = Date.now() - lastUpdateTime();
+      shouldRefresh = timeSinceLastUpdate > minutesToMs( refreshPeriodMinutes );
+    }
+
+    logger.debug( `Checking Graph DB refresh status: ${shouldRefresh}` );
+
+    if ( shouldRefresh ){
+      await clearGraphDb();
+      await populateGraphDb();
+    }
+  } catch ( err ) {
+    logger.error(`Error in Document update ${err}`);
+  }
 };
 
 export default setupGraphDbFeeds;

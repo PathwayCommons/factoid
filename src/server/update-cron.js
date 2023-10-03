454ee0ca92ed25865718bfde0e6939797d53016b
Jeffrey Wong <jvwong@gmail.com>
2023-10-03 15:49:53 -0400

    Use Timer class in Doc update. Everything in milliseconds.

--- src/server/update-cron.js
+++ src/server/update-cron.js
@@ -1,14 +1,13 @@
 import documentUpdate from './routes/api/document/update';
-import { refreshGraphDB } from './routes/api/document/graphdb';
+// import { refreshGraphDB } from './routes/api/document/graphdb';
 
-import {
-  DOCUMENT_CRON_UPDATE_PERIOD_DAYS,
-  GRAPHDB_CRON_REFRESH_PERIOD_MINUTES
-} from '../config';
+// import {
+//   GRAPHDB_CRON_REFRESH_PERIOD_MINUTES
+// } from '../config';
 
 const updateCron = async () => {
-  await documentUpdate( DOCUMENT_CRON_UPDATE_PERIOD_DAYS );
-  await refreshGraphDB( GRAPHDB_CRON_REFRESH_PERIOD_MINUTES );
+  await documentUpdate();
+  // await refreshGraphDB( GRAPHDB_CRON_REFRESH_PERIOD_MINUTES );
 };
 
 export default updateCron;

7c87809beac4592a55924417e51e92a9c525fd4b
Jeffrey Wong <jvwong@gmail.com>
2023-10-04 08:43:36 -0400

    Reenable graphdb refresh.

--- src/server/update-cron.js
+++ src/server/update-cron.js
@@ -1,13 +1,13 @@
 import documentUpdate from './routes/api/document/update';
-// import { refreshGraphDB } from './routes/api/document/graphdb';
+import { refreshGraphDB } from './routes/api/document/graphdb';
 
-// import {
-//   GRAPHDB_CRON_REFRESH_PERIOD_MINUTES
-// } from '../config';
+import {
+  GRAPHDB_CRON_REFRESH_PERIOD_MINUTES
+} from '../config';
 
 const updateCron = async () => {
   await documentUpdate();
-  // await refreshGraphDB( GRAPHDB_CRON_REFRESH_PERIOD_MINUTES );
+  await refreshGraphDB( GRAPHDB_CRON_REFRESH_PERIOD_MINUTES );
 };
 
 export default updateCron;

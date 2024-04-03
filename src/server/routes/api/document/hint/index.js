3418c011130e522f1e78b2ad791e7ee931ba68f8
Jeffrey Wong <jvwong@gmail.com>
2024-04-03 15:21:06 -0400

    Define the hints API to return organismOrdering and entities.

--- src/server/routes/api/document/hint/index.js
+++ src/server/routes/api/document/hint/index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 import logger from '../../../../logger.js';
 import pubtator from './pubtator.js';
+import organism from './organism.js';
+import { HINT_TYPE } from '../../../../../model/hint.js';
 
 const PROVIDERS = [
   pubtator
@@ -15,19 +17,37 @@ const PROVIDERS = [
  * @param {string} publicationXref.dbPrefix - The database prefix
  * @param {string} publicationXref.title - The paper title
  * @param {string} publicationXref.abstract - The paper abstract
- * @returns {Array<Hint>} An array of Hints.
+ * @returns {Object} An object with organismOrdering (sorted NCBI Taxonomy uids) and Array<Hint> of entities
  */
 async function find( publicationXref ) {
+  const isNonOrganismHint = ({ type }) =>  type !== HINT_TYPE.ORGANISM;
+  const byXref = ({ xref }) => `${xref.dbPrefix}_${xref.id}`;
   try {
-    let hints = [];
+    let hints = [],
+    entities = [],
+    organismOrdering = [];
+
+    // Compile hints from all providers
     for (const provider of PROVIDERS ){
       const providerHints = await provider.hints( publicationXref );
       if( providerHints != null ) hints = _.concat( hints, providerHints );
     }
-    return hints;
+
+    // Get an organismOrdering
+    organismOrdering = organism.order( hints );
+
+    // De-duplicated, non-Organismal hints
+    entities = hints.filter( isNonOrganismHint );
+    entities = _.uniqBy( entities, byXref );
+
+    return {
+      organismOrdering,
+      entities
+    };
 
   } catch(e) {
     logger.error(`Error in hint::find - ${JSON.stringify( publicationXref )}`);
+    throw e;
   }
 }
 
--- src/server/routes/api/document/hint/organism.js
+++ src/server/routes/api/document/hint/organism.js
@@ -1,5 +1,3 @@
-// import _ from 'lodash';
-
 import {
   HINT_TYPE,
   PASSAGE_TYPE

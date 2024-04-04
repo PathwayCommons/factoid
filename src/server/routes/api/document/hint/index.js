a5fd306e6aa77e6321a781871556e4a1138199ed
Jeffrey Wong <jvwong@gmail.com>
2024-04-04 10:46:18 -0400

    Iterate over each provider sequentially and fail silently.

--- src/server/routes/api/document/hint/index.js
+++ src/server/routes/api/document/hint/index.js
@@ -20,35 +20,33 @@ const PROVIDERS = [
  * @returns {Object} An object with organismOrdering (sorted NCBI Taxonomy uids) and Array<Hint> of entities
  */
 async function find( publicationXref ) {
-  const isNonOrganismHint = ({ type }) =>  type !== HINT_TYPE.ORGANISM;
   const byXref = ({ xref }) => `${xref.dbPrefix}_${xref.id}`;
-  try {
-    let hints = [],
-    entities = [],
-    organismOrdering = [];
+  let hints = [],
+  entities = [],
+  organismOrdering = [];
 
-    // Compile hints from all providers
-    for (const provider of PROVIDERS ){
+  // Compile hints from all providers
+  for ( const provider of PROVIDERS ){
+    try {
       const providerHints = await provider.hints( publicationXref );
       if( providerHints != null ) hints = _.concat( hints, providerHints );
+    } catch(e) {
+      logger.error('Error in hints from provider');
+      logger.error(e);
     }
-
-    // Get an organismOrdering
-    organismOrdering = organism.order( hints );
-
-    // De-duplicated, non-Organismal hints
-    entities = hints.filter( isNonOrganismHint );
-    entities = _.uniqBy( entities, byXref );
-
-    return {
-      organismOrdering,
-      entities
-    };
-
-  } catch(e) {
-    logger.error(`Error in hint::find - ${JSON.stringify( publicationXref )}`);
-    throw e;
   }
+
+  // Get an organismOrdering
+  organismOrdering = organism.order( hints );
+
+  // De-duplicate
+  // TODO: Normalize the xrefs - e.g. ATP [CHEBI:15422, MESH:68000255]
+  entities = _.uniqBy( hints, byXref );
+
+  return {
+    organismOrdering,
+    entities
+  };
 }
 
 export default { find };

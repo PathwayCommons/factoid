ec71909995d4a998bdd5b098d6e11cdfd5243f45
fileoy <alkulaib03@gmail.com>
2024-07-11 16:21:02 -0400

    added more to the hint model

--- src/model/hint.js
+++ src/model/hint.js
@@ -13,3 +13,54 @@ const HINT_PASSAGE = Object.freeze({
     ABSTRACT: 'abstract'
 });
 const HINT_PASSAGES = _.flatMap(HINT_PASSAGE);
+
+class Hint{
+
+    get text(){
+        return this._text;
+    }
+
+    set text(value){
+        this._text = value;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    set type(value){
+        if( value != HINT_TYPES.ORGANISM){
+            throw new Error('Invalid type' + value);
+        }
+        this._type = value;
+    }
+
+    get xref(){
+        return this._xref;
+    }
+
+    set xref(value){
+        if (!value.dbPrefix || !value.id){
+            throw new Error('Invalid xref' + JSON.stringify(value));
+        }
+        this._xref = value;
+    }
+
+    get section(){
+        return this._section;
+    }
+
+    set section(value){
+        if( value != HINT_PASSAGES.TITLE && value != HINT_PASSAGES.ABSTRACT){
+            throw new Error('Invalid section' + value);
+        }
+        this._section = value;
+    }
+
+
+}
+export { 
+    Hint, 
+    HINT_TYPES, 
+    HINT_PASSAGES 
+};

d0c02d4399114d89b4d949a241f4d5701b894d01
fileoy <alkulaib03@gmail.com>
2024-08-08 10:54:09 -0400

    typo

--- test/pubtator/pubtator.js
+++ test/pubtator/pubtator.js
@@ -44,7 +44,6 @@ describe('pubtator', function () {
 
         before(() => {
           hints = map(bioCDocument);
-          console.log(JSON.stringify(hints, null, 2));
         });
 
         it('Should map to a non-zero list', function () {

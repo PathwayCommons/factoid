e4ebd49610f38794f6137fa6be0af97516de6ca8
fileoy <alkulaib03@gmail.com>
2024-08-12 14:37:32 -0400

    // Contents: - PubTator API path is now configurable - get a BioCDocument from the PubTator - A simple wrapper to retrieve Hints from PubTator

--- test/pubtator/pubtator.js
+++ test/pubtator/pubtator.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 import _ from 'lodash';
+import nock from 'nock';
 
 import { Hint, HINT_TYPE, SECTION } from '../../src/model/hint.js';
-import map from '../../src/server/routes/api/document/hint/pubtator.js';
+import pubtator from '../../src/server/routes/api/document/hint/pubtator.js';
 
 import pubtator_1 from './10.1016_j.molcel.2016.11.034.json';
 import pubtator_2 from './10.1016_j.molcel.2019.03.023.json';
@@ -12,6 +13,7 @@ import pubtator_5 from './10.1126_scisignal.abf3535.json';
 import pubtator_6 from './10.1016_j.molcel.2024.01.007.json';
 import pubtator_7 from './10.15252_embj.2023113616.json';
 import pubtator_8 from './pubtator_8.json';
+import { NCBI_BASE_URL } from '../../src/config.js';
 
 /**
  * bioCDocuments
@@ -44,7 +46,7 @@ describe('pubtator', function () {
         let hints;
 
         before(() => {
-          hints = map(bioCDocument);
+          hints = pubtator.map(bioCDocument);
         });
 
         it('Should map to a non-zero list', function () {
@@ -216,4 +218,43 @@ describe('pubtator', function () {
       }); // BioC Document
     }); // forEach
   }); // map
+  
+  describe('get', function(){
+    it('Should be null when no body returned', async function(){
+      let pmid = '38496625';
+      var path = new RegExp( pmid );
+      nock( NCBI_BASE_URL )
+        .get( path )
+        .reply( 200 );
+      let bioCDocument = await pubtator.get( pmid );
+      expect( bioCDocument ).to.be.null;
+    });
+
+    it('Should exist when a body is returned', async function(){
+      let pmid = '28041912';
+      var path = new RegExp( pmid );
+      nock( NCBI_BASE_URL )
+        .get( path )
+        .reply( 200, { foo: 'bar' } );
+      let bioCDocument = await pubtator.get( pmid );
+      expect( bioCDocument ).to.exist;
+    });
+  }); // get
+
+  describe('hints', function(){
+    it('Should be null when invalid publicationXref provided', async function(){
+      let bioCDocument = await pubtator.hints( { dbPrefix: 'bar', id: 'foo'} );
+      expect( bioCDocument ).to.be.null;
+    });
+
+    it('Should exist when a body is returned', async function(){
+      let pmid = '28041912';
+      var path = new RegExp( pmid );
+      nock( NCBI_BASE_URL )
+        .get( path )
+        .reply( 200, { foo: 'bar' } );
+      let bioCDocument = await pubtator.get( pmid );
+      expect( bioCDocument ).to.exist;
+    });
+  }); // hints
 }); // pubtator

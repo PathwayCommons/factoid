696aee932123fe9e79957f80760534e9a9dee5bd
Jeffrey Wong <jvwong@gmail.com>
2024-10-31 11:10:21 -0400

    Negative controls, cases where pubmed or crossref are flaky (web search differs from web service)

--- test/util/article.js
+++ test/util/article.js
@@ -12,43 +12,57 @@ describe('article', function () {
       it('Should be true when identical', function () {
         const title = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
         const other = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should ignore casing', function () {
         const title = 'hierarchical and scaffolded phosphorylation of two degrons controls per2 stability';
         const other = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should ignore surrounding whitespace', function () {
         const title = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability   ';
         const other = '   Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should ignore surrounding periods', function () {
         const title = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability  .';
         const other = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should ignore surrounding quotations', function () {
         const title = '"Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability"';
         const other = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should be false when non-identical', function () {
         const title = 'Hierarchical and scaffolded phosphorylation of two degrons controls PER2 stability';
         const other = 'Stanniocalcin 2 governs cancer cell adaptation to nutrient insufficiency through alleviation of oxidative stress.';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.false;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.false;
+      });
+
+      it('Should be false when dissimilar (PubMed/CrossRef flaky)', function () {
+        const title = 'The mGluR6 ligand-binding domain, but not the C-terminal domain, is required for synaptic localization in retinal ON-bipolar cells';
+        const other = 'A missense mutation in Grm6 reduces but does not eliminate mGluR6 expression or rod depolarizing bipolar cell function';
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.false;
+      });
+
+      it('Should be false when dissimilar (PubMed/CrossRef flaky)', function () {
+        const title = 'Wild-type p53 oligomerizes more efficiently than p53 hot-spot mutants and overcomes mutant p53 gain-of-function via a "dominant-positive" mechanism';
+        const other = 'Differential chromatin accessibility landscape of gain-of-function mutant p53 tumours';
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.false;
       });
 
     }); // Alphabet
@@ -58,22 +72,22 @@ describe('article', function () {
       it('Should be true when identical', function () {
         const title = 'A 2-hydroxybutyrate-mediated feedback loop regulates muscular fatigue.';
         const other = 'A 2-hydroxybutyrate-mediated feedback loop regulates muscular fatigue';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should be true with varying dashes', function () {
         const title = 'Neurons enhance blood-–brain barrier function via upregulating claudin-5 and VE-cadherin expression due to glial cell line-derived neurotrophic factor secretion';
         const other = 'Neurons enhance blood brain barrier function via upregulating claudin 5 and VE cadherin expression due to glial cell line derived neurotrophic factor secretion';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
       it('Should be true with dash related to microRNA', function () {
         const title = 'Circular RNA HMGCS1 sponges MIR4521 to aggravate type 2 diabetes-induced vascular endothelial dysfunction';
         const other = 'Circular RNA HMGCS1 sponges miR-4521 to aggravate type 2 diabetes-induced vascular endothelial dysfunction';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
     }); // Punctuation
@@ -83,8 +97,8 @@ describe('article', function () {
       it('Should drop HTML tags ', function () {
         const title = '<i>Trans</i>regulation of an odorant binding protein by a proto-Y chromosome affects male courtship in house fly';
         const other = 'Transregulation of an odorant binding protein by a proto-Y chromosome affects male courtship in house fly';
-        const isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        const isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
     }); // Markup
@@ -94,10 +108,10 @@ describe('article', function () {
       it('Should be true when other includes title and vice versa', function () {
         const title = 'eLife 2024: Defining cell type-specific immune responses in a mouse model of allergic contact dermatitis by single-cell transcriptomics';
         const other = 'Defining cell type-specific immune responses in a mouse model of allergic contact dermatitis by single-cell transcriptomics';
-        let isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
-        isSame = testTitle(other, title);
-        expect( isSame ).to.be.true;
+        let isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
+        isSimilar = testTitle(other, title);
+        expect( isSimilar ).to.be.true;
       });
 
     }); // Includes
@@ -107,8 +121,8 @@ describe('article', function () {
       it('Should be true when other includes title and vice versa', function () {
         const title = '[Clinical Study of Ibrutinib Combined with Venetoclax Regimen in the Treatment of Relapsed/Refractory Diffuse Large B-Cell Lymphoma]';
         const other = '[Clinical Study of Ibrutinib Combined with Venetoclax Regimen in the Treatment of Relapsed/Refractory Diffuse Large B-Cell Lymphoma]';
-        let isSame = testTitle(title, other);
-        expect( isSame ).to.be.true;
+        let isSimilar = testTitle(title, other);
+        expect( isSimilar ).to.be.true;
       });
 
     }); // Includes

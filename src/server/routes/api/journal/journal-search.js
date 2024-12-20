90b17ce9269b6d7d6d946712ab2721d5ee430ab8
Jeffrey Wong <jvwong@gmail.com>
2024-12-20 12:16:33 -0500

    Journal endpoint with search implementation.

---
+++ src/server/routes/api/journal/journal-search.js
@@ -0,0 +1,55 @@
+import MiniSearch from 'minisearch';
+import _ from 'lodash';
+import data from './journal-list.json';
+
+/** Class for  searching an index. */
+class JournalSearch {
+
+  constructor( opts = {} ){
+    this._engine = new MiniSearch( _.assign( {}, opts, { storeFields: [ 'id' ] } ) );
+    this._documents = [];
+    this._docMap = new Map();
+  }
+
+  /**
+   * Load documents into the index and stash locally
+   * @param {object} documents List of raw documents to index
+   */
+  index( documents ){
+    this._engine.addAll( documents );
+    this._documents = documents;
+    this._documents.forEach( doc => this._docMap.set( doc.id, doc ) );
+  }
+
+  /**
+   * Getter for list of raw documents indexed
+   */
+  get documents() {
+    return this._documents;
+  }
+
+  /**
+   * Search the index
+   * @param {string} q search query
+   * @param {number} limit maximum number of hits to return (default 10)
+   * @return {object} a list of search hits (raw document)
+   */
+  search( q, limit = 10 ){
+    let searchHits = this._engine.search( q );
+    searchHits = searchHits.slice( 0, limit );
+    return searchHits.map( ({ id }) => this._docMap.get( id ) );
+  }
+}
+
+const opts = {
+  fields: ['title', 'synonyms'],
+  searchOptions: {
+    boost: { title: 2 },
+    fuzzy: 0.2
+  }
+};
+
+const journalSearch = new JournalSearch( opts );
+journalSearch.index( data );
+
+export default journalSearch;
